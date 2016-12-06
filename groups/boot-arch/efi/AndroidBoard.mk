e4ec02f337e61630e398e18ce05fa2e77fa742e3
Ragha Khandenahally <Ragha.Khandenahally@intel.com>
2016-12-06 00:28:53 -0800

    configurations and build tools for kernelflinger
    Bug ID: #https://github.com/android-ia/device-androidia/issues/2
    Test: Make sure it compiles without issues
    
    Signed-off-by: Ragha Khandenahally <Ragha.Khandenahally@intel.com>
    Tested-by: Carlos Santa <carlos.santa@intel.com>

--- groups/boot-arch/efi/AndroidBoard.mk
+++ groups/boot-arch/efi/AndroidBoard.mk
@@ -1,38 +1,9 @@
-{{#tdos}}
-BOOTLOADER_ADDITIONAL_DEPS += $(PRODUCT_OUT)/tdos.img
-BOOTLOADER_ADDITIONAL_ARGS += --bootimage $(PRODUCT_OUT)/tdos.img
-
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    TDOS_VARIANT := release
-else
-    TDOS_VARIANT := debug
-endif
-
-# FIXME move to its own repository
-tdos_ramdisk := device/intel/coho/tdos/$(TDOS_VARIANT)/ramdisk-tdos.img
-
-INSTALLED_RADIOIMAGE_TARGET += $(tdos_ramdisk) $(PRODUCT_OUT)/tdos.img
-
-$(PRODUCT_OUT)/tdos.img: \
-		$(INSTALLED_KERNEL_TARGET) \
-		$(tdos_ramdisk) \
-		$(MKBOOTIMG) $(BOOT_SIGNER) \
-
-	$(hide) mkdir -p $(dir $@)
-	$(hide) $(MKBOOTIMG) --kernel $(INSTALLED_KERNEL_TARGET) \
-			--ramdisk $(tdos_ramdisk) \
-			--cmdline "$(BOARD_KERNEL_CMDLINE)" \
-			$(BOARD_MKBOOTIMG_ARGS) \
-			--output $@
-	$(hide) $(BOOT_SIGNER) /tdos $@ $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).pk8 \
-			$(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).x509.pem $@
-{{/tdos}}
-
-
 # Rules to create bootloader zip file, a precursor to the bootloader
 # image that is stored in the target-files-package. There's also
 # metadata file which indicates how large to make the VFAT filesystem
 # image
+
+ifeq ($(BOOTLOADER_USE_PREBUILT),false)
 
 ifeq ($(TARGET_UEFI_ARCH),i386)
 efi_default_name := bootia32.efi
@@ -52,37 +23,14 @@ else
 FILE_NAME_TAG := $(BUILD_NUMBER)
 endif
 
-LOADER_PREBUILT := hardware/intel/efi_prebuilts/
-
-kernelflinger := $(PRODUCT_OUT)/efi/kernelflinger.efi
-
-ifeq ($(BOARD_USE_UEFI_SHIM),true)
-
-# EFI binaries that go in the installed device's EFI system partition
-BOARD_FIRST_STAGE_LOADER := \
-    $(LOADER_PREBUILT)/uefi_shim/$(LOADER_TYPE)/shim.efi
-
-BOARD_EXTRA_EFI_MODULES := \
-    $(LOADER_PREBUILT)/uefi_shim/$(LOADER_TYPE)/MokManager.efi \
-    $(kernelflinger)
-
-# We need kernelflinger.efi packaged inside the fastboot boot image to be
-# able to work with MCG's EFI fastboot stub
-USERFASTBOOT_2NDBOOTLOADER := $(kernelflinger)
-
-else # !BOARD_USE_UEFI_SHIM
-
-BOARD_FIRST_STAGE_LOADER := $(kernelflinger)
+BOARD_FIRST_STAGE_LOADER := $(PRODUCT_OUT)/efi/kernelflinger.efi
 BOARD_EXTRA_EFI_MODULES :=
-USERFASTBOOT_2NDBOOTLOADER :=
-endif
 
 # We stash a copy of BIOSUPDATE.fv so the FW sees it, applies the
-# update, and deletes the file. Follows Google's desire to update
-# all bootloader pieces with a single "fastboot flash bootloader"
-# command. We place the fastboot.img in the ESP for the same reason.
-# Since it gets deleted we can't do incremental updates of it, we
-# keep a copy as capsules/current.fv for this purpose.
+# update, and deletes the file. Follows Google's desire to update all
+# bootloader pieces with a single "fastboot flash bootloader" command.
+# Since it gets deleted we can't do incremental updates of it, we keep
+# a copy as capsules/current.fv for this purpose.
 intermediates := $(call intermediates-dir-for,PACKAGING,bootloader_zip)
 bootloader_zip := $(intermediates)/bootloader.zip
 $(bootloader_zip): intermediates := $(intermediates)
@@ -98,39 +46,41 @@ $(bootloader_zip): \
 	$(hide) rm -f $@
 	$(hide) mkdir -p $(efi_root)/capsules
 	$(hide) mkdir -p $(efi_root)/EFI/BOOT
-ifneq ($(BOARD_EXTRA_EFI_MODULES),)
-	$(hide) $(ACP) $(BOARD_EXTRA_EFI_MODULES) $(efi_root)/
-endif
+	$(foreach EXTRA,$(BOARD_EXTRA_EFI_MODULES), \
+		$(hide) $(ACP) $(EXTRA) $(efi_root)/)
 ifneq ($(BOARD_SFU_UPDATE),)
 	$(hide) $(ACP) $(BOARD_SFU_UPDATE) $(efi_root)/BIOSUPDATE.fv
 	$(hide) $(ACP) $(BOARD_SFU_UPDATE) $(efi_root)/capsules/current.fv
 endif
 	$(hide) $(ACP) $(BOARD_FIRST_STAGE_LOADER) $(efi_root)/loader.efi
 	$(hide) $(ACP) $(BOARD_FIRST_STAGE_LOADER) $(efi_root)/EFI/BOOT/$(efi_default_name)
-	$(hide) echo "Android-IA=\\EFI\\BOOT\\$(efi_default_name)" > $(efi_root)/manifest.txt
+	$(hide) echo "Android-Things=\\EFI\\BOOT\\$(efi_default_name)" > $(efi_root)/manifest.txt
+	$(hide) echo "Fastboot=\\EFI\\BOOT\\$(efi_default_name);-f">> $(efi_root)/manifest.txt
 	$(hide) (cd $(efi_root) && zip -qry ../$(notdir $@) .)
 
-bootloader_metadata := $(intermediates)/bootloader-size.txt
-$(bootloader_metadata):
+bootloader_info := $(intermediates)/bootloader_image_info.txt
+$(bootloader_info):
 	$(hide) mkdir -p $(dir $@)
-	$(hide) echo $(BOARD_BOOTLOADER_PARTITION_SIZE) > $@
+	$(hide) echo "size=$(BOARD_BOOTLOADER_PARTITION_SIZE)" > $@
+	$(hide) echo "block_size=$(BOARD_BOOTLOADER_BLOCK_SIZE)" >> $@
 
-INSTALLED_RADIOIMAGE_TARGET += $(bootloader_zip) $(bootloader_metadata)
+INSTALLED_RADIOIMAGE_TARGET += $(bootloader_zip) $(bootloader_info)
 
 # Rule to create $(OUT)/bootloader image, binaries within are signed with
 # testing keys
 
+BOOTLOADER_FROM_ZIP = device/intel/build/bootloader_from_zip
+
 bootloader_bin := $(PRODUCT_OUT)/bootloader
 $(bootloader_bin): \
 		$(bootloader_zip) \
-		$(MKDOSFS) \
-		$(MCOPY) \
 		$(IMG2SIMG) \
 		$(BOOTLOADER_ADDITIONAL_DEPS) \
-		device/intel/build/bootloader_from_zip \
+		$(BOOTLOADER_FROM_ZIP) \
 
-	$(hide) device/intel/build/bootloader_from_zip \
+	$(hide) $(BOOTLOADER_FROM_ZIP) \
 		--size $(BOARD_BOOTLOADER_PARTITION_SIZE) \
+		--block-size $(BOARD_BOOTLOADER_BLOCK_SIZE) \
 		$(BOOTLOADER_ADDITIONAL_ARGS) \
 		--zipfile $(bootloader_zip) \
 		$@
@@ -144,12 +94,10 @@ $(call dist-for-goals,droidcore,$(bootloader_bin):$(TARGET_PRODUCT)-bootloader-$
 fastboot_usb_bin := $(PRODUCT_OUT)/fastboot-usb.img
 $(fastboot_usb_bin): \
 		$(bootloader_zip) \
-		$(MKDOSFS) \
-		$(MCOPY) \
 		$(BOOTLOADER_ADDITIONAL_DEPS) \
-		device/intel/build/bootloader_from_zip \
+		$(BOOTLOADER_FROM_ZIP) \
 
-	$(hide) device/intel/build/bootloader_from_zip \
+	$(hide) $(BOOTLOADER_FROM_ZIP) \
 		$(BOOTLOADER_ADDITIONAL_ARGS) \
 		--zipfile $(bootloader_zip) \
 		--extra-size 10485760 \
@@ -159,39 +107,29 @@ $(fastboot_usb_bin): \
 # Build when 'make' is run with no args
 droidcore: $(fastboot_usb_bin)
 
-.PHONY: userfastboot-usb
-userfastboot-usb: $(fastboot_usb_bin)
+.PHONY: fastboot-usb
+fastboot-usb: $(fastboot_usb_bin)
 
 $(call dist-for-goals,droidcore,$(fastboot_usb_bin):$(TARGET_PRODUCT)-fastboot-usb-$(FILE_NAME_TAG).img)
-
-ifneq ($(BOARD_SFU_UPDATE),)
-$(call dist-for-goals,droidcore,$(BOARD_SFU_UPDATE):$(TARGET_PRODUCT)-sfu-$(FILE_NAME_TAG).fv)
-endif
 
 $(call dist-for-goals,droidcore,device/intel/build/testkeys/testkeys_lockdown.txt:test-keys_efi_lockdown.txt)
 $(call dist-for-goals,droidcore,device/intel/build/testkeys/unlock.txt:efi_unlock.txt)
 
-{{#fastbootefi}}
-# For fastboot-uefi we need to parse gpt.ini into
-# a binary format.
+ifeq ($(TARGET_BOOTLOADER_POLICY),$(filter $(TARGET_BOOTLOADER_POLICY),static external))
+# The bootloader policy is not built but is provided statically in the
+# repository or in $(PRODUCT_OUT)/.
+else
+# Bootloader policy values are generated based on the
+# TARGET_BOOTLOADER_POLICY value and the
+# device/intel/build/testkeys/{odm,OAK} keys.  The OEM must provide
+# its own keys.
+GEN_BLPOLICY_OEMVARS := device/intel/build/generate_blpolicy_oemvars
+TARGET_ODM_KEY_PAIR := device/intel/build/testkeys/odm
+TARGET_OAK_KEY_PAIR := device/intel/build/testkeys/OAK
 
-GPT_INI2BIN := ./device/intel/common/gpt_bin/gpt_ini2bin.py
-
-$(BOARD_GPT_BIN): $(BOARD_GPT_INI)
-	$(hide) $(GPT_INI2BIN) $< > $@
-	$(hide) echo GEN $(notdir $@)
-
-{{/fastbootefi}}
-{{^fastbootefi}}
-# for userfastboot, we need the userfastboot image in the bootloader partition.
-INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/fastboot.img
-{{/fastbootefi}}
-
-
-ifneq ($(EFI_IFWI_BIN),)
-$(call dist-for-goals,droidcore,$(EFI_IFWI_BIN):$(TARGET_PRODUCT)-ifwi-$(FILE_NAME_TAG).bin)
+$(BOOTLOADER_POLICY_OEMVARS): sign-efi-sig-list
+	$(GEN_BLPOLICY_OEMVARS) -K $(TARGET_ODM_KEY_PAIR) \
+		-O $(TARGET_OAK_KEY_PAIR).x509.pem -B $(TARGET_BOOTLOADER_POLICY) \
+		$(BOOTLOADER_POLICY_OEMVARS)
 endif
-
-ifneq ($(EFI_AFU_BIN),)
-$(call dist-for-goals,droidcore,$(EFI_AFU_BIN):$(TARGET_PRODUCT)-afu-$(FILE_NAME_TAG).bin)
 endif
