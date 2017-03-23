cbfddfc521a792ec0311e9ecd8f3097127b56d34
gkdeepa <g.k.deepa@intel.com>
2017-03-23 23:22:01 +0530

    To fix Email app crash issue
    The default density set is 160 which corresponds to mdpi,
    but the PRODUCT_AAPT_PREF_CONFIG is set as hdpi.
    Hence, changing the PRODUCT_AAPT_PREF_CONFIG to mdpi.
    
    JIRA : AIA-211
    TEST : launch of native AOSP Email app
    
    Signed-off-by: gkdeepa <g.k.deepa@intel.com>

--- groups/device-type/tablet/product.mk
+++ groups/device-type/tablet/product.mk
@@ -6,7 +6,7 @@ PRODUCT_COPY_FILES += \
         {{{tablet_core_hardware_path}}}/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
 
 PRODUCT_AAPT_CONFIG := normal large xlarge mdpi hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
 
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.sf.lcd_density=160
