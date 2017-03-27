a034ba94b02e9a42cbfe8c66c5b091aa40795bdf
Kalyan Kondapally <kalyan.kondapally@intel.com>
2017-03-26 21:11:25 -0700

    Remove redundant settings related to Bluetooth.
    This doesn't seem necessary and dont have any affect. Let's get
    rid of them.
    
    Jira: None.
    Test: Bluetooth works as before.
    
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- groups/bluetooth/btusb/product.mk
+++ groups/bluetooth/btusb/product.mk
@@ -4,6 +4,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
 		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
-
-ADDITIONAL_BUILD_PROPERTIES += bluetooth.hwcfg=stop \
-                bluetooth.rfkill=1
