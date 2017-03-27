d57596b70f46f5d4d5d732c73c58046c265ef576
amritara <amrita.raju@intel.com>
2017-03-27 12:05:12 +0530

    Enabling WiFi Direct on android_ia.
    Jira: https://01.org/jira/browse/AIA-152
    
    Test: WiFi Direct should be enabled and available peers displayed.
    Successful connection between the peers when prompted.
    
    Signed-off-by: amrita.raju@intel.com

--- groups/wlan/iwlwifi/product.mk
+++ groups/wlan/iwlwifi/product.mk
@@ -4,10 +4,11 @@ PRODUCT_PACKAGES += \
     wpa_supplicant
 
 PRODUCT_COPY_FILES += \
-  device/intel/common/wlan/wpa_supplicant-common.conf:/system/etc/wifi/wpa_supplicant.conf \
-  hardware/broadcom/wlan/bcmdhd/config/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+  device/intel/common/wlan/wpa_supplicant-common.conf:system/etc/wifi/wpa_supplicant.conf \
+  device/intel/common/wlan/iwlwifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
   frameworks/native/data/etc/android.software.app_widgets.xml:system/etc/permissions/android.software.app_widgets.xml \
-  frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+  frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+  frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
 # Wifi configuration
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
