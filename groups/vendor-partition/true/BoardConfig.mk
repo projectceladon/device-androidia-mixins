610dfcc9155aeb1b1d53f68b8013d2c46effe9a2
Harish Krupo <harish.krupo.kps@intel.com>
2017-04-14 06:16:11 +0530

    Enable squashfs compression
    Use squashfs compression for system and vendor image
    
    JIRA: AIA-113
    Test: Boots to homescreen
    
    Signed-off-by: Harish Krupo <harish.krupo.kps@intel.com>

--- groups/vendor-partition/true/BoardConfig.mk
+++ groups/vendor-partition/true/BoardConfig.mk
@@ -1,5 +1,9 @@
 # Those 3 lines are required to enable vendor image generation.
 # Remove them if vendor partition is not used.
 TARGET_COPY_OUT_VENDOR := vendor
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_PARTITION_SIZE := 524288000
+ifeq ($(SPARSE_IMG),true)
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+else
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := squashfs
+endif
