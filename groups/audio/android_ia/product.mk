362153582a4a58f2e29001f3c3f40f381d200922
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2017-04-20 16:21:18 -0500

    mixins: fix packages
    there is no audio.hdmi.android_ia package
    
    Jira: None.
    Tests: Tested on 2:1 SKL platform and no regressions.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- groups/audio/android_ia/product.mk
+++ groups/audio/android_ia/product.mk
@@ -6,9 +6,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Extended Audio HALs
 PRODUCT_PACKAGES += \
-    audio.r_submix.default \
-    audio.hdmi.android_ia \
     audio.primary.android_ia \
+    audio.r_submix.default \
     audio.usb.default \
     audio_policy.default.so \
     audio_configuration_files
