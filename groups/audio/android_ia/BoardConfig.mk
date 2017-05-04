71d547f6ff6a543db2f0d74a8b3d64ee98e776d9
Sebastien Guiriec <sebastien.guiriec@intel.com>
2017-05-04 12:46:48 +0200

    Switch to XML Audio policy format.
    Inside N release we can use XML or legacy .conf format for Audio
    policy. This patch is switching to XML format as O release supports
    only XML format.
    
    Jira: None
    
    Test: Test done Intel NUC 6i5SYH.
    
    Signed-off-by: Sebastien Guiriec <sebastien.guiriec@intel.com>

--- groups/audio/android_ia/BoardConfig.mk
+++ groups/audio/android_ia/BoardConfig.mk
@@ -5,3 +5,5 @@ BOARD_USES_GENERIC_AUDIO ?= false
 #  INTEL_AUDIO_HAL:= audio     -> baseline HAL
 #  INTEL_AUDIO_HAL:= audio_pfw -> PFW-based HAL
 INTEL_AUDIO_HAL := audio
+# Use XML audio policy configuration file
+USE_XML_AUDIO_POLICY_CONF ?= 1
