16119b4b73273a3167e073a22031ece4398da3ae
Sebastien Guiriec <sebastien.guiriec@intel.com>
2017-05-09 13:12:03 +0200

    Add INTEL_AUDIO_HAL selection flag default setting.
    In order to be able to switch between baseline and Paramerter
    Framework Audio HAL the flag INTEL_AUDIO_HAL needs to be propagated to
    Baseline HAL.
    
    Jira: None
    
    Test: Build on latest manifest of the 2 different Audio stacks.
    
    Signed-off-by: Sebastien Guiriec <sebastien.guiriec@intel.com>

--- groups/audio/android_ia/BoardConfig.mk
+++ groups/audio/android_ia/BoardConfig.mk
@@ -1,3 +1,7 @@
 BOARD_USES_ALSA_AUDIO := true
 BOARD_USES_TINY_ALSA_AUDIO := true
 BOARD_USES_GENERIC_AUDIO ?= false
+# Audio HAL selection Flag default setting.
+#  INTEL_AUDIO_HAL:= audio     -> baseline HAL
+#  INTEL_AUDIO_HAL:= audio_pfw -> PFW-based HAL
+INTEL_AUDIO_HAL := audio
