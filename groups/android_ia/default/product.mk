94a4d072dac0319f00aceffdf4488d5fb388835a
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2017-06-15 23:34:11 +0530

    Remove not supported features
    Jira: None
    Test: Boot, Wifi browsing, app navigation
    
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- groups/android_ia/default/product.mk
+++ groups/android_ia/default/product.mk
@@ -35,50 +35,21 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
 
-# Camera: Device-specific configuration files.
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-
 # Voip
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
-
-$(foreach sensor,accelerometer barometer compass gyroscope light stepcounter stepdetector proximity, \
-$(eval PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.$(sensor).xml:system/etc/permissions/android.hardware.sensor.$(sensor).xml))
 
 # Usb
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
-# NFC
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
-
-# Gps
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-
 # Touch
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-
-#Sensors
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
 
 # USB
 PRODUCT_COPY_FILES += \
@@ -90,12 +61,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.texture_cache_size=24.0f \
     ro.hwui.text_large_cache_width=2048 \
     ro.hwui.text_large_cache_height=512
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10 \
-    keyguard.no_require_sim=true \
-    ro.com.android.dataroaming=true
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.rtc_local_time=1 \
@@ -111,17 +76,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Property to choose between virtual/external wfd display
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.wfd.virtual=0
-
-# Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    camera.disable_zsl_mode=0 \
-    persist.camera.HAL3.enabled=0 \
-    persist.camera.ois.disable=0
-
-# Camera: Format set up for graphics
-PRODUCT_PROPERTY_OVERRIDES += ro.camera.pixel_format = 0x10F
-PRODUCT_PROPERTY_OVERRIDES += ro.camera.rec.pixel_format = 0x100
-PRODUCT_PROPERTY_OVERRIDES += ro.ycbcr.pixel_format = 0x10F
 
 # Input resampling configuration
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -149,25 +103,14 @@ PRODUCT_PACKAGES += \
     WallpaperPicker \
     NotePad \
     Provision \
-    camera.android_ia \
     drmserver \
-    gps.default \
-    lights.default \
     power.android_ia \
     scp \
-    sensors.hsb \
     sftp \
     ssh \
     sshd \
     local_time.default.so \
-    vibrator.default.so \
     keystore.default.so
-
-# Sensors
-PRODUCT_PACKAGES += \
-    sensorhubd      \
-    libsensorhub    \
-    AndroidCalibrationTool \
 
 # USB
 PRODUCT_PACKAGES += \
