b2e3090a96912f4098d7771bf6099c9799a9d0ea
Harish Krupo <harish.krupo.kps@intel.com>
2017-12-08 11:34:59 +0530

    Add services required for light hal
    Jira: None
    Test: Brightness change should work in settings
    
    Signed-off-by: Harish Krupo <harish.krupo.kps@intel.com>

--- groups/lights/true/product.mk
+++ groups/lights/true/product.mk
@@ -1,1 +1,7 @@
-PRODUCT_PACKAGES += lights.android_ia
+# Lights HAL
+BOARD_SEPOLICY_DIRS += \
+    device/intel/android_ia/sepolicy/light
+
+PRODUCT_PACKAGES += lights.android_ia \
+    android.hardware.light@2.0-service \
+    android.hardware.light@2.0-impl
