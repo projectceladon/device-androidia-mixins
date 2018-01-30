5bea52b3a606861045f22e6a92234e91f749eda3
Aiswarya Cyriac <aiswarya.cyriac@intel.com>
2018-01-30 16:08:10 +0530

    [AIA-O] Add mixin changes for Bluetooth
    Jira: None
    Test: None
    
    Signed-off-by: Atul Vaish <atul.vaish@intel.com>
    Signed-off-by: Aiswarya Cyriac <aiswarya.cyriac@intel.com>

--- groups/bluetooth/btusb/product.mk
+++ groups/bluetooth/btusb/product.mk
@@ -1,6 +1,6 @@
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
-		ath3k-1.fw \
+    ath3k-1.fw
 
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
 		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
