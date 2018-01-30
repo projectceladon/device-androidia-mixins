096f2b385d222ed836110bec14105ba8c934bf3a
Aiswarya Cyriac <aiswarya.cyriac@intel.com>
2018-01-30 16:08:10 +0530

    [AIA-O] Add mixin changes for Bluetooth
    Jira: None
    Test: None
    
    Signed-off-by: Atul Vaish <atul.vaish@intel.com>
    Signed-off-by: Aiswarya Cyriac <aiswarya.cyriac@intel.com>

--- groups/bluetooth/btusb/BoardConfig.mk
+++ groups/bluetooth/btusb/BoardConfig.mk
@@ -3,4 +3,4 @@ BOARD_HAVE_BLUETOOTH_LINUX := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/intel/common/bluetooth/bcm43241/
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-bt-pan
 BOARD_SEPOLICY_DIRS += device/intel/android_ia/sepolicy/bluetooth/common \
-                       device/intel/android_ia/sepolicy/bluetooth/btusb
+                       device/intel/android_ia/sepolicy/bluetooth/intel
