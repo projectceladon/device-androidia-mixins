eed80a843987672041774f2e5cc3e23529d665f1
Ming Tan <ming.tan@intel.com>
2018-01-31 12:39:13 +0800

    Delete the prefix of partition name 'android_' and file fstab_byname.
    In Android O, the default partition name should not have prefix.
    Shoule use kernelflinger 5.02 or later version.
    
    Jira: None.
    Test: Test it in Joule and KBL NUC, can boot to UI.
    
    Signed-off-by: Ming Tan <ming.tan@intel.com>

--- groups/boot-arch/android_ia/product.mk
+++ groups/boot-arch/android_ia/product.mk
@@ -1,4 +1,4 @@
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/by-name/android_persistent
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/by-name/persistent
 
 ifeq ($(TARGET_BOOTLOADER_POLICY),$(filter $(TARGET_BOOTLOADER_POLICY),0x0 0x2 0x4 0x6))
 # OEM Unlock reporting 1
