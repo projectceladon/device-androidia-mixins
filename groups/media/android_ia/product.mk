58f22071b48556244e38b3fde262a4a36bf896bb
Priya Shree <priya.shree@intel.com>
2018-02-09 13:49:09 +0530

    [AIA-O] Adding PRODUCT_COPY_FILES for media_codecs_performance.xml
    Jira: None
    Test: Device boots to homescreen
    
    Signed-off-by: Priya Shree <priya.shree@intel.com>

--- groups/media/android_ia/product.mk
+++ groups/media/android_ia/product.mk
@@ -12,7 +12,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     device/intel/android_ia/common/media/media_profiles.xml:system/etc/media_profiles.xml \
-    device/intel/android_ia/common/media/media_codecs.xml:system/etc/media_codecs.xml
+    device/intel/android_ia/common/media/media_codecs.xml:system/etc/media_codecs.xml \
+    device/intel/android_ia/common/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 #Enable deep buffer for video playback
 PRODUCT_PROPERTY_OVERRIDES += media.stagefright.audio.deep=true
