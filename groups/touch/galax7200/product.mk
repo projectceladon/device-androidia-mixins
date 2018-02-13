29526a6b298668ad115100e4b208637ef4bc0cba
saranya <saranya.gopal@intel.com>
2018-02-13 11:21:20 +0530

    [AIA-O] Touch: Add support for EGalax touch screen
    This patch creates mixin support to enable eGalax 7200
    touch screen.
    
    Jira: None
    Test: None
    
    Signed-off-by: saranya <saranya.gopal@intel.com>

---
+++ groups/touch/galax7200/product.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:vendor/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml\
+        device/intel/common/touch/Vendor_0eef_Product_7200.idc:system/usr/idc/Vendor_0eef_Product_7200.idc
