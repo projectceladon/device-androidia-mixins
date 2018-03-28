ce1aabd9fed6a8cb1de3180a8366ad4e49bebdfb
swei22 <w.sheng@intel.com>
2018-03-28 19:18:56 +0800

    Enable trusty hardware backed keymaster feature
    Enable hardware backed keymaster.
    
    Jira: None
    Test: Device boots to home screen.
    Signed-off-by: swei22 <w.sheng@intel.com>

--- groups/trusty/true/product.mk
+++ groups/trusty/true/product.mk
@@ -2,6 +2,13 @@
 
 KM_VERSION := {{{keymaster_version}}}
 
+ifeq ($(KM_VERSION),2)
+PRODUCT_PACKAGES += \
+	keystore.trusty
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.hardware.keystore=trusty
+endif
+
 ifeq ($(KM_VERSION),1)
 PRODUCT_PACKAGES += \
 	keystore.android_ia
