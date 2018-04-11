79350e2db0261eaf3a6f87cc19d26fb6f3aadfa3
karanx <karanx.bhagoji@intel.com>
2018-04-11 16:46:25 +0530

    [CELADON] Patch to add Car device type.
    This patch adds the new mixins device-type 'car'
    
    Jira: None
    Test: None
    
    Signed-off-by: Karan889 <karanx.bhagoji@intel.com>
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

---
+++ groups/device-type/car/BoardConfig.mk
@@ -0,0 +1,7 @@
+
+BOARD_SEPOLICY_DIRS += \
+    packages/services/Car/car_product/sepolicy \
+    device/generic/car/common/sepolicy \
+    device/intel/project-celadon/sepolicy/car
+
+TARGET_USES_CAR_FUTURE_FEATURES := true
