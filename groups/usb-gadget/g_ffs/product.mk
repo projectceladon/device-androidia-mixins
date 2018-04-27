7cab4208e1a1dadadb2bbc2c7475bbfd62286e7d
K, Gururaj <gururaj.k@intel.com>
2018-04-27 11:51:19 +0530

    usb: gadget: disable secure adb for eng builds
    disable secure adb for eng builds to unblock test automation
    
    Jira: None
    Test: None
    
    signed-off-by: K, Gururaj <gururaj.k@intel.com>

--- groups/usb-gadget/g_ffs/product.mk
+++ groups/usb-gadget/g_ffs/product.mk
@@ -3,17 +3,11 @@ USB_CONFIG := mtp
 ifeq ($(TARGET_BUILD_VARIANT),user)
 # Enable Secure Debugging
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
+PRODUCT_COPY_FILES += device/intel/common/usb-gadget/adb_keys:root/adb_keys
 ifeq ($(BUILD_FOR_CTS_AUTOMATION),true)
 # Build for automated CTS
 USB_CONFIG := $(USB_CONFIG),adb
-PRODUCT_COPY_FILES += device/intel/common/usb-gadget/adb_keys:root/adb_keys
 endif #BUILD_FOR_CTS_AUTOMATION == true
 endif #TARGET_BUILD_VARIANT == user
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=$(USB_CONFIG)
 
-# Add Intel adb keys for userdebug/eng builds
-ifneq ($(TARGET_BUILD_VARIANT),user)
-# Enable Secure Debugging
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
-PRODUCT_COPY_FILES += device/intel/common/usb-gadget/adb_keys:root/adb_keys
-endif
