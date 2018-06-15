8f8dddf5b63b9d1989b78157d8c24598787c64b8
Ming Tan <ming.tan@intel.com>
2018-06-15 10:12:34 +0800

    Add the gptbuild group, it is used to create a full disk image.
    The full disk image can be used for USB live boot image, and simulate
    system.
    
    Jira: https://01.org/jira/browse/CEL-13
    Test: Test it in KBL NUC.
    
    The original author is Charles Johnson <charles.f.johnson@intel.com>.
    
    Signed-off-by: Charles Johnson <charles.f.johnson@intel.com>
    Signed-off-by: Perrot, ThomasX <thomasx.perrot@intel.com>
    Signed-off-by: roger feng <roger.feng@intel.com>
    Signed-off-by: Fu Yonghua <yonghuax.fu@intel.com>
    Signed-off-by: zhouji3x <jianfengx.zhou@intel.com>
    Signed-off-by: Chen Lin Z <lin.z.chen@intel.com>
    Signed-off-by: Chen, Xihua <xihua.chen@intel.com>
    Signed-off-by: Regnier, Philippe <philippe.regnier@intel.com>
    Signed-off-by: Xihua Chen <xihua.chen@intel.com>
    Signed-off-by: Yang, Kai <kaix.yang@intel.com>
    Signed-off-by: kwen <kui.wen@intel.com>
    Signed-off-by: yanqinlx <yanqinx.liu@intel.com>
    Signed-off-by: Yang Zhenwei <zhenweix.yang@intel.com>
    Signed-off-by: Qi, Yadong <yadong.qi@intel.com>
    Signed-off-by: Ming Tan <ming.tan@intel.com>

---
+++ groups/gptbuild/doc.spec
@@ -0,0 +1,29 @@
+=== Overview
+
+gptbuild is used to configure gptbuild support
+
+--- deps
+
+    - trusty
+    - vendor-partition
+
+==== Options
+
+--- true
+this option is used to add gptbuild support
+
+    --- parameters
+        acrn_data : true for saving gptimage to acrn_data partition with ext4 filesystem
+        data_size: size of ext4 filesystem
+    --- code dir
+
+
+--- false
+this option is used to not add gptbuild support
+
+    --- parameters
+
+    --- code dir
+
+--- default
+when not explicitly selected in mixin spec file, the default option will be used.
---
+++ groups/gptbuild/true/AndroidBoard.mk
@@ -0,0 +1,101 @@
+gptimage_size ?= {{{size}}}
+
+raw_config := none
+raw_factory := none
+tos_bin := none
+multiboot_bin := none
+
+.PHONY: none
+none: ;
+
+.PHONY: $(INSTALLED_CONFIGIMAGE_TARGET).raw
+$(INSTALLED_CONFIGIMAGE_TARGET).raw: $(INSTALLED_CONFIGIMAGE_TARGET) $(SIMG2IMG)
+	$(SIMG2IMG) $< $@
+
+.PHONY: $(INSTALLED_FACTORYIMAGE_TARGET).raw
+$(INSTALLED_FACTORYIMAGE_TARGET).raw: $(INSTALLED_FACTORYIMAGE_TARGET) $(SIMG2IMG)
+	$(SIMG2IMG) $< $@
+
+ifdef INSTALLED_CONFIGIMAGE_TARGET
+raw_config := $(INSTALLED_CONFIGIMAGE_TARGET).raw
+endif
+
+ifdef INSTALLED_FACTORYIMAGE_TARGET
+raw_factory := $(INSTALLED_FACTORYIMAGE_TARGET).raw
+endif
+
+.PHONY: $(GPTIMAGE_BIN)
+ifeq ($(strip $(TARGET_USE_TRUSTY)),true)
+ifeq ($(strip $(TARGET_USE_MULTIBOOT)),true)
+$(GPTIMAGE_BIN): tosimage multiboot
+multiboot_bin = $(INSTALLED_MULTIBOOT_IMAGE_TARGET)
+else
+$(GPTIMAGE_BIN): tosimage
+endif
+tos_bin = $(INSTALLED_TOS_IMAGE_TARGET)
+endif
+
+$(GPTIMAGE_BIN): \
+	bootloader \
+	bootimage \
+	{{^slot-ab}}
+	recoveryimage \
+	cacheimage \
+	{{/slot-ab}}
+	systemimage \
+	{{#avb}}
+	vbmetaimage \
+	{{/avb}}
+    {{#vendor-partition}}
+	vendorimage \
+    {{/vendor-partition}}
+	$(SIMG2IMG) \
+	$(raw_config) \
+	$(raw_factory)
+
+	$(hide) rm -f $(INSTALLED_SYSTEMIMAGE).raw
+	$(hide) rm -f $(INSTALLED_USERDATAIMAGE_TARGET).raw
+	{{^slot-ab}}
+	$(hide) rm -f $(INSTALLED_CACHEIMAGE_TARGET).raw
+	{{/slot-ab}}
+
+	$(MAKE_EXT4FS) \
+		-l $(BOARD_USERDATAIMAGE_PARTITION_SIZE) -L data \
+		$(PRODUCT_OUT)/userdata.dummy
+
+	$(SIMG2IMG) $(INSTALLED_SYSTEMIMAGE) $(INSTALLED_SYSTEMIMAGE).raw
+	{{^slot-ab}}
+	$(SIMG2IMG) $(INSTALLED_CACHEIMAGE_TARGET) $(INSTALLED_CACHEIMAGE_TARGET).raw
+	{{/slot-ab}}
+    {{#vendor-partition}}
+	$(SIMG2IMG) $(INSTALLED_VENDORIMAGE_TARGET) $(INSTALLED_VENDORIMAGE_TARGET).raw
+    {{/vendor-partition}}
+
+	$(INTEL_PATH_BUILD)/create_gpt_image.py \
+		--create $@ \
+		--block $(BOARD_FLASH_BLOCK_SIZE) \
+		--table $(BOARD_GPT_INI) \
+		--size $(gptimage_size) \
+		--bootloader $(bootloader_bin) \
+		--bootloader2 $(bootloader_bin) \
+		--tos $(tos_bin) \
+		--multiboot $(multiboot_bin) \
+		--boot $(INSTALLED_BOOTIMAGE_TARGET) \
+		{{^slot-ab}}
+		--recovery $(INSTALLED_RECOVERYIMAGE_TARGET) \
+		--cache $(INSTALLED_CACHEIMAGE_TARGET).raw \
+		{{/slot-ab}}
+		{{#avb}}
+		--vbmeta $(INSTALLED_VBMETAIMAGE_TARGET) \
+		{{/avb}}
+		--system $(INSTALLED_SYSTEMIMAGE).raw \
+        {{#vendor-partition}}
+		--vendor $(INSTALLED_VENDORIMAGE_TARGET).raw \
+        {{/vendor-partition}}
+		--data $(PRODUCT_OUT)/userdata.dummy \
+		--config $(raw_config) \
+		--factory $(raw_factory)
+
+
+.PHONY: gptimage
+gptimage: $(GPTIMAGE_BIN)
