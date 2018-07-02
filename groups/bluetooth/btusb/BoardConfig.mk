ad3292749aaa06b1fb02cb2f9fe4a00e8a11609b
Umesh Agarwal <umeshx.agarwal@intel.com>
2018-07-02 14:01:38 +0530

    Summary: - Selection of Bluetooth Build Config header file   based on device-type (car or tablet)
    Description:
    - Declared a flag "ivi" in option.spec under bluetooth/btusb
      and set its default value "false".
    - Set "BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR" based on
      the flag "ivi" in BoardConfig.mk file.
    
    Jira: https://jira01.devtools.intel.com/browse/OAM-65576
    
    Test:
    - BT AVRCP should work, user shouble be able to control music
      from Bluetooth Audio App on kabylake IVI - PASS
    
    Signed-off-by: Umesh Agarwal <umeshx.agarwal@intel.com>

--- groups/bluetooth/btusb/BoardConfig.mk
+++ groups/bluetooth/btusb/BoardConfig.mk
@@ -1,6 +1,12 @@
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_LINUX := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/intel/common/bluetooth/bcm43241/
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-bt-pan
 BOARD_SEPOLICY_DIRS += device/intel/project-celadon/sepolicy/bluetooth/common \
                        device/intel/project-celadon/sepolicy/bluetooth/intel
+{{#ivi}}
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/intel/common/bluetooth/intel/car/
+{{/ivi}}
+
+{{^ivi}}
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/intel/common/bluetooth/intel/tablet/
+{{/ivi}}
