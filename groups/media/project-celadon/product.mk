20a1af5c1db9e45d377a590e9f92e46497747c34
TianmiChen <tianmi.chen@intel.com>
2018-07-25 18:07:16 +0800

    [Celadon] Add option to enable media driver
    Jira: None
    Test: None
    
    Signed-off-by: TianmiChen <tianmi.chen@intel.com>

--- groups/media/project-celadon/product.mk
+++ groups/media/project-celadon/product.mk
@@ -2,17 +2,17 @@
 PRODUCT_PACKAGES += \
     libva \
     libva-android \
-    libva-egl \
-    libva-tpi \
     i965_drv_video
+
+PRODUCT_PACKAGES += libpciaccess
 
 # Audio/video codec support.
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    device/intel/project-celadon/common/media/media_profiles.xml:system/etc/media_profiles.xml \
-    device/intel/project-celadon/common/media/media_codecs.xml:system/etc/media_codecs.xml \
-    device/intel/project-celadon/common/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:vendor/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:vendor/etc/media_codecs_google_video.xml \
+    device/intel/project-celadon/common/media/media_profiles.xml:vendor/etc/media_profiles.xml \
+    device/intel/project-celadon/common/media/media_codecs.xml:vendor/etc/media_codecs.xml \
+    device/intel/project-celadon/common/media/media_codecs_performance.xml:vendor/etc/media_codecs_performance.xml
 
 #Enable deep buffer for video playback
 PRODUCT_PROPERTY_OVERRIDES += media.stagefright.audio.deep=true
@@ -21,9 +21,7 @@ PRODUCT_PROPERTY_OVERRIDES += media.stagefright.audio.deep=true
 PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true
 
-{{#mediasdk}}
 # Enable Media Sdk
-
 PRODUCT_PACKAGES += \
     libmfxhw32 \
     libmfxhw64 \
@@ -32,5 +30,4 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 PRODUCT_COPY_FILES += \
-    device/intel/project-celadon/common/media/mfx_omxil_core.conf:system/etc/mfx_omxil_core.conf
-{{/mediasdk}}
+    device/intel/project-celadon/common/media/mfx_omxil_core.conf:vendor/etc/mfx_omxil_core.conf
