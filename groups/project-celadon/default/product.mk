ca7bc016dafc7124de6da3d27846f8e375190d45
Swaroop Balan <swaroop.balan@intel.com>
2018-08-08 12:46:21 +0530

    [CELADON] Change locales_full.mk to languages_full.mk
    Tracked-On : None

--- groups/project-celadon/default/product.mk
+++ groups/project-celadon/default/product.mk
@@ -27,7 +27,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Sceptrum.ogg \
 
 # Get a list of languages.
-$(call inherit-product,$(SRC_TARGET_DIR)/product/locales_full.mk)
+#$(call inherit-product,$(SRC_TARGET_DIR)/product/locales_full.mk)
+$(call inherit-product,$(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get everything else from the parent package
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
