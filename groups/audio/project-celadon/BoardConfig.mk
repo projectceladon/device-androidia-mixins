c7943733e041c82635292289df87e4afd2445500
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2018-08-27 15:41:40 +0530

    audio: Add sepolicy directory
    Due to missing sepolicies for audio, audio server is getting
    restarted and as a result bluetooth service also gets restarted.
    
    Add sepolicies for audio to get audio and bluetooth working.
    
    Tracked-On: OAM-67968
    
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- groups/audio/project-celadon/BoardConfig.mk
+++ groups/audio/project-celadon/BoardConfig.mk
@@ -14,3 +14,5 @@ endif
 USE_XML_AUDIO_POLICY_CONF ?= 1
 # Use configurable audio policy
 USE_CONFIGURABLE_AUDIO_POLICY ?= 1
+
+BOARD_SEPOLICY_DIRS += device/intel/project-celadon/sepolicy/audio/project-celadon
