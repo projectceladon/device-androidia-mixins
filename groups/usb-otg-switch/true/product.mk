6c77a6d0ccb27f5ba320aab6de16d0dd276cad07
saranya <saranya.gopal@intel.com>
2018-11-05 19:32:53 +0530

    Fix the path of usb-otg-switch script
    Jira: None
    
    Signed-off-by: saranya <saranya.gopal@intel.com>

--- groups/usb-otg-switch/true/product.mk
+++ groups/usb-otg-switch/true/product.mk
@@ -1,1 +1,1 @@
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/{{_extra_dir}}/usb_otg_switch.sh:vendor/bin/usb_otg_switch.sh
+PRODUCT_COPY_FILES += device/intel/mixins/groups/usb-otg-switch/true/usb_otg_switch.sh:vendor/bin/usb_otg_switch.sh
