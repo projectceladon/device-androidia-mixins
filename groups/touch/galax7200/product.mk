9bf1aa7a18b4211f24c2da72d3f159c014c984e6
raghupatelraj <rugnath.ram@intel.com>
2018-11-09 14:24:48 +0530

    Celadon porting mixins [touch]
    Tracked-On: OAM-71487
    Signed-off-by: raghupatelraj <rugnath.ram@intel.com>
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- groups/touch/galax7200/product.mk
+++ groups/touch/galax7200/product.mk
@@ -1,3 +1,3 @@
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:vendor/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml\
-        device/intel/common/touch/Vendor_0eef_Product_7200.idc:system/usr/idc/Vendor_0eef_Product_7200.idc
+        $(INTEL_PATH_COMMON)/touch/Vendor_0eef_Product_7200.idc:system/usr/idc/Vendor_0eef_Product_7200.idc
