cf01f85c0e757c530e740c65bb8cf5532e4f4db1
raghupatelraj <rugnath.ram@intel.com>
2018-11-09 10:01:56 +0530

    Celadon porting mixins [lights]
    Tracked-On: OAM-71487
    Signed-off-by: raghupatelraj <rugnath.ram@intel.com>
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- groups/lights/true/product.mk
+++ groups/lights/true/product.mk
@@ -1,7 +1,8 @@
 # Lights HAL
 BOARD_SEPOLICY_DIRS += \
-    device/intel/project-celadon/sepolicy/light
+    $(INTEL_PATH_SEPOLICY)/light
 
-PRODUCT_PACKAGES += lights.project-celadon \
+PRODUCT_PACKAGES += lights.$(TARGET_BOARD_PLATFORM) \
     android.hardware.light@2.0-service \
     android.hardware.light@2.0-impl
+
