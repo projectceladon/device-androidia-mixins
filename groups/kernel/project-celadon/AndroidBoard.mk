88d04b428df5ae1cf45c45daf1c98db8ae7c4659
sgnanase <sundar.gnanasekaran@intel.com>
2018-11-12 14:04:59 +0530

    [CELADON] Update kernel mixins
    Tracked-On: OAM-71470
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- groups/kernel/project-celadon/AndroidBoard.mk
+++ groups/kernel/project-celadon/AndroidBoard.mk
@@ -1,72 +1,196 @@
-ifneq ($(TARGET_PREBUILT_KERNEL),)
-$(error TARGET_PREBUILT_KERNEL defined but AndroidIA kernels build from source)
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+
+LOCAL_KERNEL_PATH := $(abspath $(PRODUCT_OUT)/obj/kernel)
+KERNEL_INSTALL_MOD_PATH := .
+LOCAL_KERNEL := $(LOCAL_KERNEL_PATH)/arch/x86/boot/bzImage
+LOCAL_KERNEL_MODULE_TREE_PATH := $(LOCAL_KERNEL_PATH)/lib/modules
+KERNELRELEASE := $(shell cat $(LOCAL_KERNEL_PATH)/include/config/kernel.release)
+
+KERNEL_CCACHE := $(realpath $(CC_WRAPPER))
+
+#remove time_macros from ccache options, it breaks signing process
+KERNEL_CCSLOP := $(filter-out time_macros,$(subst $(comma), ,$(CCACHE_SLOPPINESS)))
+KERNEL_CCSLOP := $(subst $(space),$(comma),$(KERNEL_CCSLOP))
+
+
+ifeq ($(DEV_BKC_KERNEL), true)
+  LOCAL_KERNEL_SRC :=
+  KERNEL_CONFIG_PATH :=
+  EXT_MODULES :=
+  DEBUG_MODULES :=
+
+else ifeq ($(LTS2018_KERNEL), true)
+  LOCAL_KERNEL_SRC :=
+  KERNEL_CONFIG_PATH :=
+  EXT_MODULES :=
+  DEBUG_MODULES :=
+
+else
+  LOCAL_KERNEL_SRC := kernel/project-celadon
+  EXT_MODULES :=
+  DEBUG_MODULES :=
+  KERNEL_CONFIG_PATH := kernel/config-lts/v4.19/project-celadon/android
 endif
 
-TARGET_KERNEL_SRC ?= kernel/project-celadon
+EXTMOD_SRC := ../modules
+EXTERNAL_MODULES := $(EXT_MODULES)
 
-TARGET_KERNEL_ARCH := x86_64
-TARGET_KERNEL_CONFIG ?= kernel_64_defconfig
+KERNEL_DEFCONFIG := $(KERNEL_CONFIG_PATH)/$(TARGET_KERNEL_ARCH)_defconfig
+ifneq ($(TARGET_BUILD_VARIANT), user)
+  KERNEL_DEBUG_DIFFCONFIG += $(wildcard $(KERNEL_CONFIG_PATH)/debug_diffconfig)
+  ifneq ($(KERNEL_DEBUG_DIFFCONFIG),)
+    KERNEL_DIFFCONFIG += $(KERNEL_DEBUG_DIFFCONFIG)
+  else
+    KERNEL_DEFCONFIG := $(LOCAL_KERNEL_SRC)/arch/x86/configs/$(TARGET_KERNEL_ARCH)_debug_defconfig
+  endif
+  EXTERNAL_MODULES := $(EXT_MODULES) $(DEBUG_MODULES)
+endif # variant not eq user
 
-KERNEL_CONFIG_DIR := {{{kernel_config_dir}}}
+KERNEL_CONFIG := $(LOCAL_KERNEL_PATH)/.config
 
-KERNEL_NAME := bzImage
+ifeq ($(TARGET_BUILD_VARIANT), eng)
+  KERNEL_ENG_DIFFCONFIG := $(wildcard $(KERNEL_CONFIG_PATH)/eng_diffconfig)
+  ifneq ($(KERNEL_ENG_DIFFCONFIG),)
+    KERNEL_DIFFCONFIG += $(KERNEL_ENG_DIFFCONFIG)
+  endif
+endif
 
-# Set the output for the kernel build products.
-KERNEL_OUT := $(abspath $(TARGET_OUT_INTERMEDIATES)/kernel)
-KERNEL_BIN := $(KERNEL_OUT)/arch/$(TARGET_KERNEL_ARCH)/boot/$(KERNEL_NAME)
-KERNEL_MODULES_INSTALL := $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/lib/modules
+KERNEL_MAKE_OPTIONS = \
+    SHELL=/bin/bash \
+    -C $(LOCAL_KERNEL_SRC) \
+    O=$(LOCAL_KERNEL_PATH) \
+    ARCH=$(TARGET_KERNEL_ARCH) \
+    INSTALL_MOD_PATH=$(KERNEL_INSTALL_MOD_PATH) \
+    CROSS_COMPILE="$(KERNEL_CCACHE) $(YOCTO_CROSSCOMPILE)" \
+    CCACHE_SLOPPINESS=$(KERNEL_CCSLOP)
 
-KERNELRELEASE = $(shell cat $(KERNEL_OUT)/include/config/kernel.release)
+KERNEL_MAKE_OPTIONS += \
+    EXTRA_FW="$(_EXTRA_FW_)" \
+    EXTRA_FW_DIR="$(abspath $(PRODUCT_OUT)/vendor/firmware)"
 
-KMOD_OUT := $(shell readlink -f "$(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)")
+KERNEL_CONFIG_DEPS = $(strip $(KERNEL_DEFCONFIG) $(KERNEL_DIFFCONFIG))
+KERNEL_CONFIG_MK := $(LOCAL_KERNEL_PATH)/config.mk
+-include $(KERNEL_CONFIG_MK)
 
-build_kernel := $(MAKE) -C $(TARGET_KERNEL_SRC) \
-		O=$(KERNEL_OUT) \
-		ARCH=$(TARGET_KERNEL_ARCH) \
-		CROSS_COMPILE="$(KERNEL_CROSS_COMPILE_WRAPPER)" \
-		KCFLAGS="$(KERNEL_CFLAGS)" \
-		KAFLAGS="$(KERNEL_AFLAGS)" \
-		$(if $(SHOW_COMMANDS),V=1) \
-		INSTALL_MOD_PATH=$(KMOD_OUT)
+ifneq ($(KERNEL_CONFIG_DEPS),$(KERNEL_CONFIG_PREV_DEPS))
+.PHONY: $(KERNEL_CONFIG)
+endif
 
-KERNEL_CONFIG_FILE := device/intel/project-celadon/kernel_config/$(TARGET_KERNEL_CONFIG)
+CHECK_CONFIG_SCRIPT := $(LOCAL_KERNEL_SRC)/scripts/diffconfig
+CHECK_CONFIG_LOG :=  $(LOCAL_KERNEL_PATH)/.config.check
 
-KERNEL_CONFIG := $(KERNEL_OUT)/.config
-$(KERNEL_CONFIG): $(KERNEL_CONFIG_FILE)
-	$(hide) mkdir -p $(@D) && cat $(wildcard $^) > $@
-	$(build_kernel) oldnoconfig
+KERNEL_DEPS := $(shell find $(LOCAL_KERNEL_SRC) \( -name *.git -prune \) -o -print )
 
-# Produces the actual kernel image!
-$(PRODUCT_OUT)/kernel: $(KERNEL_CONFIG) | $(ACP)
-	$(build_kernel) $(KERNEL_NAME) modules
-	$(hide) $(ACP) -fp $(KERNEL_BIN) $@
+# Before building final defconfig with debug diffconfigs
+# Check that base defconfig is correct. Check is performed
+# by comparing generated .config with .config.old
+# If differences are observed, display a help message
+# and stop kernel build.
+# If a .config is already present, save it before processing
+# the check and restore it at the end
+$(CHECK_CONFIG_LOG): $(KERNEL_DEFCONFIG) $(KERNEL_DEPS)
+	$(hide) mkdir -p $(@D)
+	-$(hide) [[ -e $(KERNEL_CONFIG) ]] && mv -f $(KERNEL_CONFIG) $(KERNEL_CONFIG).save
+	$(hide) cat $< > $(KERNEL_CONFIG)
+	$(hide) $(MAKE) $(KERNEL_MAKE_OPTIONS) olddefconfig
+	$(hide) $(CHECK_CONFIG_SCRIPT) $(KERNEL_CONFIG).old $(KERNEL_CONFIG) > $@
+	-$(hide) [[ -e $(KERNEL_CONFIG).save ]] && mv -f $(KERNEL_CONFIG).save $(KERNEL_CONFIG)
+	$(hide) if [[ -s $@ ]] ; then \
+	  echo "CHECK KERNEL DEFCONFIG FATAL ERROR :" ; \
+	  echo "Kernel config copied from $(KERNEL_DEFCONFIG) has some config issue." ; \
+ 	  echo "Final '.config' and '.config.old' differ. This should never happen." ; \
+	  echo "Observed diffs are :" ; \
+	  cat $@ ; \
+	  echo "Root cause is probably that a dependancy declared in Kconfig is not respected" ; \
+	  echo "or config was added in Kconfig but value not explicitly added to defconfig." ; \
+	  echo "Recommanded method to generate defconfig is menuconfig tool instead of manual edit." ; \
+	  exit 1;  fi;
 
-EXTMOD_SRC := ../../../../../..
+menuconfig xconfig gconfig: $(CHECK_CONFIG_LOG)
+	$(hide) xterm -e $(MAKE) $(KERNEL_MAKE_OPTIONS) $@
+	$(hide) cp -f $(KERNEL_CONFIG) $(KERNEL_DEFCONFIG)
+	@echo ===========
+	@echo $(KERNEL_DEFCONFIG) has been modified !
+	@echo ===========
 
-ALL_EXTRA_MODULES := $(patsubst %,$(TARGET_OUT_INTERMEDIATES)/kmodule/%,$(TARGET_EXTRA_KERNEL_MODULES))
-$(ALL_EXTRA_MODULES): $(TARGET_OUT_INTERMEDIATES)/kmodule/%: $(PRODUCT_OUT)/kernel
-	@echo Building additional kernel module $*
-	$(build_kernel) M=$(abspath $@) modules
+$(KERNEL_CONFIG): $(KERNEL_CONFIG_DEPS)
+	$(hide) echo "KERNEL_CONFIG_PREV_DEPS := $(KERNEL_CONFIG_DEPS)" > $(KERNEL_CONFIG_MK)
+	$(hide) cat $(KERNEL_CONFIG_DEPS) > $@
+	@echo "Generating Kernel configuration, using $(KERNEL_CONFIG_DEPS)"
+	$(hide) $(MAKE) $(KERNEL_MAKE_OPTIONS) olddefconfig </dev/null
+
+$(PRODUCT_OUT)/kernel: $(LOCAL_KERNEL) $(LOCAL_KERNEL_PATH)/copy_modules
+	$(hide) cp $(LOCAL_KERNEL) $@
+
+# kernel modules must be copied before vendorimage is generated
+$(PRODUCT_OUT)/vendor.img: $(LOCAL_KERNEL_PATH)/copy_modules
 
 # Copy modules in directory pointed by $(KERNEL_MODULES_ROOT)
 # First copy modules keeping directory hierarchy lib/modules/`uname-r`for libkmod
 # Second, create flat hierarchy for insmod linking to previous hierarchy
-$(KERNEL_MODULES_INSTALL): $(PRODUCT_OUT)/kernel $(ALL_EXTRA_MODULES)
-	$(hide) rm -rf $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/lib/modules
-	$(build_kernel) modules_install
-	$(hide) for kmod in $(TARGET_EXTRA_KERNEL_MODULES) ; do \
-		echo Installing additional kernel module $${kmod} ; \
-		$(subst +,,$(subst $(hide),,$(build_kernel))) M=$(abspath $(TARGET_OUT_INTERMEDIATES))/kernel/$${kmod} modules_install ; \
-	done
-	$(hide) rm -f $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/lib/modules/*/{build,source}
-	$(hide) mv $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/lib/modules/$(KERNELRELEASE)/* $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/lib/modules
-	$(hide) rm -rf $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/lib/modules/$(KERNELRELEASE)
+$(LOCAL_KERNEL_PATH)/copy_modules: $(LOCAL_KERNEL)
+	@echo Copy modules from $(LOCAL_KERNEL_PATH)/lib/modules/$(KERNELRELEASE) into $(PRODUCT_OUT)/$(KERNEL_MODULES_ROOT)
+	$(hide) rm -rf $(PRODUCT_OUT)/$(KERNEL_MODULES_ROOT)
+	$(hide) rm -rf $(TARGET_RECOVERY_ROOT_OUT)/$(KERNEL_MODULES_ROOT)
+	$(hide) mkdir -p $(PRODUCT_OUT)/$(KERNEL_MODULES_ROOT)
+	$(hide) cd $(LOCAL_KERNEL_PATH)/lib/modules/$(KERNELRELEASE) && for f in `find . -name '*.ko' -or -name 'modules.*'`; do \
+	        cp $$f $(PWD)/$(PRODUCT_OUT)/$(KERNEL_MODULES_ROOT)/$$(basename $$f) || exit 1; \
+	        mkdir -p $(PWD)/$(PRODUCT_OUT)/$(KERNEL_MODULES_ROOT)/$(KERNELRELEASE)/$$(dirname $$f) ; \
+	        ln -s /$(KERNEL_MODULES_ROOT_PATH)/$$(basename $$f) $(PWD)/$(PRODUCT_OUT)/$(KERNEL_MODULES_ROOT)/$(KERNELRELEASE)/$$f || exit 1; \
+	        done
 	$(hide) touch $@
+#usb-init for recovery
+	$(hide) mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/$(KERNEL_MODULES_ROOT)
+	$(hide) for f in dwc3.ko dwc3-pci.ko xhci-hcd.ko xhci-pci.ko; do \
+	        find $(LOCAL_KERNEL_PATH)/lib/modules/ -name $$f -exec cp {} $(TARGET_RECOVERY_ROOT_OUT)/$(KERNEL_MODULES_ROOT)/ \; ;\
+	        done
+#mei for recovery
+	$(hide) for f in mei.ko mei-me.ko; do \
+	        find $(LOCAL_KERNEL_PATH)/lib/modules/ -name $$f -exec cp {} $(TARGET_RECOVERY_ROOT_OUT)/$(KERNEL_MODULES_ROOT)/ \; ;\
+	        done
 
-# Makes sure any built modules will be included in the system image build.
-ALL_DEFAULT_INSTALLED_MODULES += $(KERNEL_MODULES_INSTALL)
+$(LOCAL_KERNEL): $(MINIGZIP) $(KERNEL_CONFIG) $(BOARD_DTB) $(KERNEL_DEPS)
+	$(MAKE) $(KERNEL_MAKE_OPTIONS)
+	$(MAKE) $(KERNEL_MAKE_OPTIONS) modules
+	$(MAKE) $(KERNEL_MAKE_OPTIONS) INSTALL_MOD_STRIP=1 modules_install
 
-installclean: FILES += $(KERNEL_OUT) $(PRODUCT_OUT)/kernel
 
+# disable the modules built in parallel due to some modules symbols has dependency,
+# and module install depmod need they be installed one at a time.
+
+PREVIOUS_KERNEL_MODULE := $(LOCAL_KERNEL)
+
+define bld_external_module
+
+$(eval MODULE_DEPS_$(2) := $(shell find kernel/modules/$(1) \( -name *.git -prune \) -o -print ))
+
+$(LOCAL_KERNEL_PATH)/build_$(2): $(LOCAL_KERNEL) $(MODULE_DEPS_$(2)) $(PREVIOUS_KERNEL_MODULE)
+	@echo BUILDING $(1)
+	@mkdir -p $(LOCAL_KERNEL_PATH)/../modules/$(1)
+	$(hide) $(MAKE) $$(KERNEL_MAKE_OPTIONS) M=$(EXTMOD_SRC)/$(1) V=1 $(ADDITIONAL_ARGS_$(subst /,_,$(1))) modules
+	@touch $$(@)
+
+$(LOCAL_KERNEL_PATH)/install_$(2): $(LOCAL_KERNEL_PATH)/build_$(2) $(PREVIOUS_KERNEL_MODULE)
+	@echo INSTALLING $(1)
+	$(hide) $(MAKE) $$(KERNEL_MAKE_OPTIONS) M=$(EXTMOD_SRC)/$(1) INSTALL_MOD_STRIP=1 modules_install
+	@touch $$(@)
+
+$(LOCAL_KERNEL_PATH)/copy_modules: $(LOCAL_KERNEL_PATH)/install_$(2)
+
+$(eval PREVIOUS_KERNEL_MODULE := $(LOCAL_KERNEL_PATH)/install_$(2))
+endef
+
+
+# Check external module path
+$(foreach m,$(EXTERNAL_MODULES),$(if $(findstring .., $(m)), $(error $(m): All external kernel modules should be put under kernel/modules folder)))
+
+$(foreach m,$(EXTERNAL_MODULES),$(eval $(call bld_external_module,$(m),$(subst /,_,$(m)))))
+
+
+
+# Add a kernel target, so "make kernel" will build the kernel
 .PHONY: kernel
-kernel: $(PRODUCT_OUT)/kernel
+kernel: $(LOCAL_KERNEL_PATH)/copy_modules $(PRODUCT_OUT)/kernel
+
+
+endif
