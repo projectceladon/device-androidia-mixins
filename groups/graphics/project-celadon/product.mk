ed10da1c2d8bb8399956e5112f11bacec643b7cf
Shwetha B <shwetha.b@intel.com>
2018-11-16 14:52:52 +0530

    Update product.mk

--- groups/graphics/project-celadon/product.mk
+++ groups/graphics/project-celadon/product.mk
@@ -58,7 +58,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 {{#vulkan}}
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml
-
 {{/vulkan}}
 
 {{/gen9+}}
