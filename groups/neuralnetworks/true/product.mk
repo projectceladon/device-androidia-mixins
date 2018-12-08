777339b0111be8daaab0d537380475f1e35031fe
asingh26 <arun.kumar.singh@intel.com>
2018-12-08 19:08:53 +0530

    adding android nn hal service
    Tracked-On:
    Signed-off-by: asingh26 <arun.kumar.singh@intel.com>

--- groups/neuralnetworks/true/product.mk
+++ groups/neuralnetworks/true/product.mk
@@ -1,3 +1,27 @@
-# neuralnetworks sample HAL on CPU 
+# neuralnetworks HAL
 PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.1-service-sample-all
+    android.hardware.neuralnetworks@1.0-service-sample-all \
+
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.0-generic-service \
+    android.hardware.neuralnetworks@1.0-generic-impl
+
+PRODUCT_PACKAGES += \
+    libinference_engine
+
+#PRODUCT_PACKAGES += \
+#    libMKLDNNPlugin \
+#    libmkldnn
+
+PRODUCT_PACKAGES += \
+    libmyriadPlugin \
+    libmvnc
+
+PRODUCT_PACKAGES += \
+    hello_ncs2 \
+    graphtest_vpu \
+    graphtest_cpu
+
+#PRODUCT_PACKAGES += \
+#    android.hardware.neuralnetworks@1.0-vpu-service \
+#    android.hardware.neuralnetworks@1.0-vpu-impl
