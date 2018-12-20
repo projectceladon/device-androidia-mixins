a5e75fa9967d7f23f443fbf9d465487b9c7b3c1e
Meng Xianglin <xianglinx.meng@intel.com>
2018-12-20 08:23:10 +0800

    Add groups and files for early mount
    mixin-update is modified to handle .asl file
    
    Tracked-On: OAM-74948
    Signed-off-by: Meng Xianglin <xianglinx.meng@intel.com>

---
+++ groups/product-partition/true/BoardConfig.mk
@@ -0,0 +1,9 @@
+# Those 3 lines are required to enable product image generation.
+# Remove them if product partition is not used.
+TARGET_COPY_OUT_PRODUCT := product
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := {{system_fs}}
+BOARD_PRODUCTIMAGE_PARTITION_SIZE := $(shell echo {{partition_size}}*1048576 | bc)
+TARGET_USE_PRODUCT := true
+{{#slot-ab}}
+AB_OTA_PARTITIONS += product
+{{/slot-ab}}
