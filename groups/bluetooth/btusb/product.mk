13e3901da4eacc26c9ddd929f0307e135e362dcf
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2018-12-24 13:06:48 +0530

    Changes to make treble complaint
    - Move vendor specific files to vendor partition
    - Replace google bluetooth vendor hidl service to Intel bluetooth
    vendor specific service.
    
    Tracked-On: OAM-73774
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- groups/bluetooth/btusb/product.mk
+++ groups/bluetooth/btusb/product.mk
@@ -2,13 +2,12 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     ath3k-1.fw
 
-PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:vendor/etc/permissions/android.hardware.bluetooth.xml \
+		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:vendor/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
-  android.hardware.bluetooth@1.0-impl \
-  android.hardware.bluetooth@1.0-service \
+  android.hardware.bluetooth@1.0-service.vbt \
   libbt-vendor
 
 {{#ivi}}
