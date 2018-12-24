cb17c449dfa5d20745c6f01a7707b47faa6f54bf
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2018-12-24 11:38:10 +0530

    bluetooth: Set BOARD_HAVE_BLUETOOTH_LINUX_PRI to true
    Remove setting of BOARD_HAVE_BLUETOOTH and
    BOARD_HAVE_BLUETOOTH_LINUX to true.
    
    Set BOARD_HAVE_BLUETOOTH_LINUX_PRI to true to make use
    of bluetooth vendor adaptation.
    
    Tracked-On: OAM-73759
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- groups/bluetooth/btusb/BoardConfig.mk
+++ groups/bluetooth/btusb/BoardConfig.mk
@@ -1,5 +1,4 @@
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_LINUX := true
+BOARD_HAVE_BLUETOOTH_LINUX_PRI := true
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-bt-pan
 BOARD_SEPOLICY_DIRS += device/intel/project-celadon/sepolicy/bluetooth/common \
                        device/intel/project-celadon/sepolicy/bluetooth/intel
