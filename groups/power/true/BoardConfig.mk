5e7cd1feb00f1079685befb6948de875f031ef32
Madhusudhan S <madhusudhan.s@intel.com>
2019-01-10 11:45:40 +0530

    [DO-NOT-MERGE] PowerHAL 1.2-Adding mixins config for sepolicy
    Signed-off-by: Madhusudhan S <madhusudhan.s@intel.com>

---
+++ groups/power/true/BoardConfig.mk
@@ -0,0 +1,4 @@
+BOARD_SEPOLICY_DIRS += device/intel/project-celadon/sepolicy/power
+#DEVICE_FRAMEWORK_MANIFEST_FILE += \
+				system/libhidl/vintfdata/manifest_healthd_exclude.xml
+
---
+++ groups/power/true/product.mk
@@ -0,0 +1,3 @@
+PRODUCT_PACKAGES += power
+PRODUCT_PACKAGES += power.$(TARGET_BOARD_PLATFORM) \
+					android.hardware.power@1.2-service.celadon
