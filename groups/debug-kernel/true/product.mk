5a1942090f348377c210fac60c0afc43740ac278
rnaidu <ramya.v.naidu@intel.com>
2019-01-31 09:22:11 -0500

    Aligning the file structure for mixins groups
    Tracked-On: OAM-76011
    Signed-off-by: rnaidu <ramya.v.naidu@intel.com>

--- groups/debug-kernel/true/product.mk
+++ groups/debug-kernel/true/product.mk
@@ -1,3 +1,3 @@
 ifneq ($(TARGET_BUILD_VARIANT),user)
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/init.kernel.rc:root/init.kernel.rc
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/{{_extra_dir}}/init.kernel.rc:root/init.kernel.rc
 endif
