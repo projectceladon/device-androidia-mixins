ac2943c92e7699701c6093e08d793c41a45c3baf
avaish1 <atul.vaish@intel.com>
2019-02-06 15:16:59 +0530

    Update product.mk
    Tracked-On: OAM-76130
    Signed-off-by: Atul Vaish <atul.vaish@intel.com>

--- groups/neuralnetworks/true/product.mk
+++ groups/neuralnetworks/true/product.mk
@@ -1,3 +1,20 @@
-# neuralnetworks sample HAL on CPU 
+# neuralnetworks HAL for HW Accelerators
 PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.1-service-sample-all
+    android.hardware.neuralnetworks@1.0-generic-service \
+    android.hardware.neuralnetworks@1.0-generic-impl
+
+PRODUCT_PACKAGES += \
+    libinference_engine
+
+PRODUCT_PACKAGES += \
+    libMKLDNNPlugin \
+    libmkldnn
+
+PRODUCT_PACKAGES += \
+    libmyriadPlugin \
+    libmvnc
+
+PRODUCT_PACKAGES += \
+    hello_ncs2 \
+    graphtest_vpu \
+    graphtest_cpu
