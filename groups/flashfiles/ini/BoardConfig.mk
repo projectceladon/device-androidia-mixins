7db238d53f5f3d2fea907bb4fc866c036d2edf6f
rnaidu <ramya.v.naidu@intel.com>
2019-02-06 10:02:08 +0530

    Changing the filestructure for flashfiles to use the extra directory
    Tracked-On: OAM-76022
    Signed-off-by: rnaidu <ramya.v.naidu@intel.com>

--- groups/flashfiles/ini/BoardConfig.mk
+++ groups/flashfiles/ini/BoardConfig.mk
@@ -1,4 +1,4 @@
-FLASHFILES_CONFIG ?= $(TARGET_DEVICE_DIR)/flashfiles.ini
+FLASHFILES_CONFIG ?= $(TARGET_DEVICE_DIR)/{{_extra_dir}}/flashfiles.ini
 USE_INTEL_FLASHFILES := true
 VARIANT_SPECIFIC_FLASHFILES ?= false
 FAST_FLASHFILES := {{fast_flashfiles}}
