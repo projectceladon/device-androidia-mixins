861ab26c74ac98def2a7876700f4f53a2b61b296
Meng Xianglin <xianglinx.meng@intel.com>
2019-02-07 06:21:17 +0530

    Enable early mount for project-celadon
    Tracked-On: OAM-69202
    Signed-off-by: Meng Xianglin <xianglinx.meng@intel.com>

--- groups/firststage-mount/true/AndroidBoard.mk
+++ groups/firststage-mount/true/AndroidBoard.mk
@@ -1,4 +1,4 @@
-FIRST_STAGE_MOUNT_CFG_FILE := $(TARGET_DEVICE_DIR)/config.asl
+FIRST_STAGE_MOUNT_CFG_FILE := $(TARGET_DEVICE_DIR)/extra_files/firststage-mount/config.asl
 
 $(FIRSTSTAGE_MOUNT_SSDT): $(FIRST_STAGE_MOUNT_CFG_FILE) $(IASL)
 	$(hide) $(IASL) -p $(@:.aml=) $(FIRST_STAGE_MOUNT_CFG_FILE);
