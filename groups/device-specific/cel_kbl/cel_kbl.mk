c81222d7b095ec37f12f7a5c05ec0a72b4c69c16
Ming Tan <ming.tan@intel.com>
2019-02-12 14:20:26 +0530

    [TREBLE] Turn on treble flag for cel_apl
    Tracked-On: None
    Signed-off-by: Qiming Shi <qiming.shi@intel.com>
    Signed-off-by: Ming Tan <ming.tan@intel.com>

--- groups/device-specific/cel_kbl/cel_kbl.mk
+++ groups/device-specific/cel_kbl/cel_kbl.mk
@@ -3,6 +3,9 @@ ENABLE_NATIVEBRIDGE_64BIT := true
 $(call inherit-product,$(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product,device/intel/project-celadon/cel_kbl/device.mk)
 
+include build/make/target/product/treble_common.mk
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+
 # Overrides
 PRODUCT_NAME := cel_kbl
 PRODUCT_BRAND := cel_kbl
