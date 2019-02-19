6b49e84c9133972691e258a4de85a899c48585b6
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2019-02-19 15:51:12 +0530

    iwlwifi: Remove wlan_intel_restore as its not used
    Tracked-On: OAM-76216
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- groups/wlan/iwlwifi/option.spec
+++ groups/wlan/iwlwifi/option.spec
@@ -1,6 +1,5 @@
 [defaults]
 iwl_pnvm_hw = DEFAULT
-gpp = false
 softap_dualband_allow = false
 iwl_sub_folder = rel
 libwifi-hal = true
--- groups/wlan/iwlwifi/product.mk
+++ groups/wlan/iwlwifi/product.mk
@@ -24,13 +24,6 @@ PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.wifi.xml:vendor/etc/permissions/android.hardware.wifi.xml \
         frameworks/native/data/etc/android.hardware.wifi.direct.xml:vendor/etc/permissions/android.hardware.wifi.direct.xml
 
-{{#gpp}}
-# Add Manufacturing tool
-PRODUCT_PACKAGES += \
-    wlan_intel_restore.sh
-{{/gpp}}
-
-
 # Wifi configuration
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WLAN_DEVICE := iwlwifi
