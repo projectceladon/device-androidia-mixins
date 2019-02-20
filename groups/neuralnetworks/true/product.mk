d4cf8d2c2f7c98af6c9a3169034170700f3586f6
Atul Vaish <atul.vaish@intel.com>
2019-02-20 20:40:41 +0530

    Mixin Configuration for NNHAL and OPENVINO
    Tracked-On: OAM-76388
    Signed-off-by: Atul Vaish <atul.vaish@intel.com>

--- groups/neuralnetworks/true/product.mk
+++ groups/neuralnetworks/true/product.mk
@@ -1,3 +1,15 @@
-# neuralnetworks sample HAL on CPU 
+# neuralnetworks HAL
 PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.1-service-sample-all
+    android.hardware.neuralnetworks@1.0-generic-service \
+    android.hardware.neuralnetworks@1.0-generic-impl
+
+PRODUCT_PACKAGES += \
+    libinference_engine
+
+PRODUCT_PACKAGES += \
+    libMKLDNNPlugin\
+    libmkldnn
+
+PRODUCT_PACKAGES += \
+    graphtest_cpu
+
