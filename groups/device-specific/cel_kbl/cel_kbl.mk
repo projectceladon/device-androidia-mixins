3903b45a7758cc87fa8677148969b1648cfb5624
swaroopb <swaroop.balan@intel.com>
2019-02-25 15:26:21 +0530

    [CELADON] Updated the mixins for gms/gas
    Tracked-On: None
    Signed-off-by: swaroopb <swaroop.balan@intel.com>

--- groups/device-specific/cel_kbl/cel_kbl.mk
+++ groups/device-specific/cel_kbl/cel_kbl.mk
@@ -1,10 +1,17 @@
 ENABLE_NATIVEBRIDGE_64BIT := true
 
 $(call inherit-product,$(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product,device/intel/project-celadon/cel_kbl/device.mk)
 
 include build/make/target/product/treble_common.mk
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+
+# Include GAS application.
+# Must put this calling before device.mk to make sure GAS overlay take
+# precedence than Car product overlay
+FLAG_GAS_MINIMAL := true
+FLAG_GAS_AVAILABLE := true
+$(call inherit-product-if-exists, vendor/google/gas/products/gas.mk)
+$(call inherit-product,device/intel/project-celadon/cel_kbl/device.mk)
 
 # Overrides
 PRODUCT_NAME := cel_kbl
