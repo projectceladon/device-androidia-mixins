34965e1a2f2f6afb2cbc61ff6729e840e370117e
Hongcheng Xie <hongcheng.xie@intel.com>
2019-02-26 14:58:06 +0800

    Add default drm mixin group
    Tracked-On: OAM-76272
    Signed-off-by: Hongcheng Xie <hongcheng.xie@intel.com>
    Signed-off-by: Youhua Zhu <youhuax.zhu@intel.com>

---
+++ groups/default-drm/true/product.mk
@@ -0,0 +1,5 @@
+#only enable default drm service
+PRODUCT_PACKAGES += android.hardware.drm@1.0-service \
+                    android.hardware.drm@1.0-impl \
+                    android.hardware.drm@1.1-service.clearkey
+
