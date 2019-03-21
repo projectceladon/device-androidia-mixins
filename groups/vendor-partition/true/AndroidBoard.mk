9819d737ea910adf8f7657f30f1d49810804fd29
Regnier, Philippe <philippe.regnier@intel.com>
2019-03-21 17:14:38 +0800

    ota: add sh in recovery mode
    Tracked-On: OAM-78373
    Signed-off-by: Regnier, Philippe <philippe.regnier@intel.com>

--- groups/vendor-partition/true/AndroidBoard.mk
+++ groups/vendor-partition/true/AndroidBoard.mk
@@ -1,3 +1,7 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE := vendor-partition
+LOCAL_REQUIRED_MODULES := toybox_static
+include $(BUILD_PHONY_PACKAGE)
 
 # This is to ensure that kernel modules are installed before
 # vendor.img is generated.
@@ -9,3 +13,19 @@ make_dir_ab_vendor:
 
 $(PRODUCT_OUT)/ramdisk.img: make_dir_ab_vendor
 {{/slot-ab}}
+
+RECOVERY_VENDOR_LINK_PAIRS := \
+	$(PRODUCT_OUT)/recovery/root/vendor/bin/getprop:toolbox_static \
+
+RECOVERY_VENDOR_LINKS := \
+	$(foreach item, $(RECOVERY_VENDOR_LINK_PAIRS), $(call word-colon, 1, $(item)))
+
+$(RECOVERY_VENDOR_LINKS):
+	$(hide) echo "Creating symbolic link on $(notdir $@)"
+	$(eval PRV_TARGET := $(call word-colon, 2, $(filter $@:%, $(RECOVERY_VENDOR_LINK_PAIRS))))
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(dir $@)$(PRV_TARGET))
+	$(hide) touch $(dir $@)$(PRV_TARGET)
+	$(hide) ln -sf $(PRV_TARGET) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(RECOVERY_VENDOR_LINKS)
