7af0dbef2b48ea429d3487cb581535d485965d61
rnaidu <ramya.v.naidu@intel.com>
2019-06-11 11:21:54 +0530

    Mixin porting for camera similar to apl_nuc Q
    Tracked-On: N/A
    Signed-off-by: rnaidu <ramya.v.naidu@intel.com>

--- groups/camera-ext/ext-camera-only/BoardConfig.mk
+++ groups/camera-ext/ext-camera-only/BoardConfig.mk
@@ -1,1 +1,5 @@
-BOARD_SEPOLICY_DIRS += device/intel/project-celadon/sepolicy/camera-ext/ext-camera-only
+# Enable only USB camera and disable all CSI Cameras
+BOARD_CAMERA_USB_STANDALONE = true
+
+# SELinux support for USB camera
+BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/camera-ext/ext-camera-only
