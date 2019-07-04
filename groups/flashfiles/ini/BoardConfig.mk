f944d16325cbd84dc97f5a48da76dbb080b6f7fa
jialei-feng <jialei.feng@outlook.com>
2019-07-04 14:07:42 +0800

    Update BoardConfig.mk

--- groups/flashfiles/ini/BoardConfig.mk
+++ groups/flashfiles/ini/BoardConfig.mk
@@ -1,7 +1,6 @@
 FLASHFILES_CONFIG ?= $(TARGET_DEVICE_DIR)/flashfiles.ini
 USE_INTEL_FLASHFILES := true
 VARIANT_SPECIFIC_FLASHFILES ?= false
-BOARD_FLASHFILES += $(PRODUCT_OUT)/obj/kernel/arch/x86/boot/compressed/vmlinux.bin:vmlinux.bin
 {{#fast_flashfiles}}
 FAST_FLASHFILES := true
 {{/fast_flashfiles}}
