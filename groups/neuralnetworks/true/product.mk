43893b8f4ccc2e4dce6300cccb58e2c6a8d995f9
Nagamani Chennuboina <nagamani.chennuboina@intel.com>
2019-07-15 21:45:31 +0530

    NNhal 1.0 with MKLDNN and without openvino
    Tracked-On:
    Signed-off-by: Nagamani Chennuboina <nagamani.chennuboina@intel.com>

--- groups/neuralnetworks/true/product.mk
+++ groups/neuralnetworks/true/product.mk
@@ -1,16 +1,17 @@
 # neuralnetworks HAL
 PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.0-generic-service \
-    android.hardware.neuralnetworks@1.0-generic-impl \
-    android.hardware.neuralnetworks@1.0-service-gpgpu
+    android.hardware.neuralnetworks@1.0-service-gpgpu \
+    android.hardware.neuralnetworks@1.0-mkldnn-service
 
 PRODUCT_PACKAGES += \
     libinference_engine
 
 PRODUCT_PACKAGES += \
-    libMKLDNNPlugin\
     libmkldnn
 
 PRODUCT_PACKAGES += \
     graphtest_cpu
 
+PRODUCT_PROPERTY_OVERRIDES += debug.nn.vlog=0
+PRODUCT_PROPERTY_OVERRIDES += debug.nn.cpuonly=0
+PRODUCT_PROPERTY_OVERRIDES += debug.nn.partition=1
