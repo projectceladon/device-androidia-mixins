3fec2750b8c589af50d8bc648f07da705fcc0b40
Zhen Han <zhen.han@intel.com>
2019-08-09 16:16:32 +0800

    Mixin: Enable Thermal HAL Version 2.0
    Change-Id: I7a9aec70116faa9e6f9660504eeff6e6c92b5650
    Tracked-On: OAM-84757
    Signed-off-by: Sun Xinx <xinx.sun@intel.com>
    Reviewed-on: 677520

--- groups/thermal/thermal-daemon/product.mk
+++ groups/thermal/thermal-daemon/product.mk
@@ -4,11 +4,5 @@ PRODUCT_COPY_FILES += \
 	$(INTEL_PATH_COMMON)/thermal/thermal-daemon/thermal-conf.xml:/vendor/etc/thermal-daemon/thermal-conf.xml \
 	$(INTEL_PATH_COMMON)/thermal/thermal-daemon/thermal-cpu-cdev-order.xml:/vendor/etc/thermal-daemon/thermal-cpu-cdev-order.xml
 
-{{#ioc}}
-IOC_ENABLED := true
-THERMAL_HAL_IOC_TYPE += {{ioc}}
-{{/ioc}}
 # Thermal Hal
-PRODUCT_PACKAGES += thermal.$(TARGET_BOARD_PLATFORM) \
-                    android.hardware.thermal@2.0-service.intel \
-                    android.hardware.thermal@2.0-impl.intel
+PRODUCT_PACKAGES += android.hardware.thermal@2.0-service.intel
