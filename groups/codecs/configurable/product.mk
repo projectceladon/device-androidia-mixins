7bc406fadeccee41e3b1c7175500a1e0b56818c1
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2019-08-26 08:22:35 +0530

    Add media_profiles.xml with 1080p support
    - Added media_profiles.xml with 1080p support
    - Added 1080p_support option to copy media_profiles.xml
    based on 1080p_support option setting.
    - Updated default values for camera.disable_treble and
    media.settings.xml in system.prop file and removed
    from init.rc.
    
    Change-Id: I2119dbcbdf82449af55c8820357433cf4eb080c6
    Tracked-On: OAM-85187
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
    Signed-off-by: Nikumbh, Mayur NimbaX <mayur.nimbax.nikumbh@intel.com>
    Reviewed-on: 678553

--- groups/codecs/configurable/product.mk
+++ groups/codecs/configurable/product.mk
@@ -2,9 +2,9 @@
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:vendor/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:vendor/etc/media_codecs_google_video.xml \
-    $(LOCAL_PATH)/{{_extra_dir}}/media_profiles.xml:vendor/etc/media_profiles.xml \
     $(LOCAL_PATH)/{{_extra_dir}}/media_codecs.xml:vendor/etc/media_codecs.xml \
-    $(LOCAL_PATH)/{{_extra_dir}}/mfx_omxil_core.conf:vendor/etc/mfx_omxil_core.conf
+    $(LOCAL_PATH)/{{_extra_dir}}/mfx_omxil_core.conf:vendor/etc/mfx_omxil_core.conf \
+    $(LOCAL_PATH)/{{_extra_dir}}/{{profile_file}}:vendor/etc/media_profiles.xml
 
 {{^codec_perf_xen}}
 PRODUCT_COPY_FILES += \
