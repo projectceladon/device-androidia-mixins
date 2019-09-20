334a978b9ecbd78750590140c257499de74c951a
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2019-09-20 10:57:22 +0530

    Dont set the value of "media.settings.xml"
    Currently, this property is system non-exported. So, can't be set
    neither in vendor partition nor in system.prop as
    VtsValidateMediaProfiles with GSI(Generic System Image) will
    fail.
    
    Copy media_profiles file as media_profiles_V1_0.xml to
    vendor/etc folder and clean setting of the property from
    system.prop.
    
    Tracked-On: OAM-85140
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
    Signed-off-by: Tong Bo <bo.tong@intel.com>

--- groups/codecs/configurable/product.mk
+++ groups/codecs/configurable/product.mk
@@ -4,7 +4,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:vendor/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/{{_extra_dir}}/media_codecs.xml:vendor/etc/media_codecs.xml \
     $(LOCAL_PATH)/{{_extra_dir}}/mfx_omxil_core.conf:vendor/etc/mfx_omxil_core.conf \
-    $(LOCAL_PATH)/{{_extra_dir}}/{{profile_file}}:vendor/etc/media_profiles.xml
+    $(LOCAL_PATH)/{{_extra_dir}}/{{profile_file}}:vendor/etc/media_profiles_V1_0.xml
 
 {{^codec_perf_xen}}
 PRODUCT_COPY_FILES += \
