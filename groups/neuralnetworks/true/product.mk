0739abd95d09f4a784d71a2b845972849ecd4bb3
Fei Jiang <fei.jiang@intel.com>
2019-09-28 16:15:21 +0800

    add nn gpu hal support in mixin
    Tracked-On: OAM-86881
    Signed-off-by: Fei Jiang <fei.jiang@intel.com>

--- groups/neuralnetworks/true/product.mk
+++ groups/neuralnetworks/true/product.mk
@@ -1,7 +1,8 @@
 # neuralnetworks HAL
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.1-generic-service \
-    android.hardware.neuralnetworks@1.1-generic-impl
+    android.hardware.neuralnetworks@1.1-generic-impl \
+    android.hardware.neuralnetworks@1.2-service-gpgpu
 
 PRODUCT_PACKAGES += \
     libinference_engine
