4aa05b91947e4fa9ddd5e4eff05d18cbd6b51c70
Mirche,Dinesh <dinesh.mirche@intel.com>
2019-10-14 08:01:54 +0530

    Enable pfw on celadon
    Tracked-On:
    Signed-off-by: Mirche,Dinesh <dinesh.mirche@intel.com>

--- groups/audio/project-celadon/product.mk
+++ groups/audio/project-celadon/product.mk
@@ -1,5 +1,3 @@
-TARGET_BOARD_PLATFORM := broxton
-
 # Tinyalsa
 PRODUCT_PACKAGES_DEBUG += \
     tinymix \
@@ -11,58 +9,21 @@ PRODUCT_PACKAGES_DEBUG += \
 # Audio HALs
 PRODUCT_PACKAGES += meta.package.audio
 
-# Sound Trigger HAL
-PRODUCT_PACKAGES += \
-       sound_trigger.primary.$(TARGET_BOARD_PLATFORM)
-
-# Audio Primary HAL
-PRODUCT_PACKAGES += \
-       audio.primary.$(TARGET_BOARD_PLATFORM)
-
-# Extended Audio HALs
-PRODUCT_PACKAGES += \
-    audio.r_submix.default \
-    audio.usb.default \
-    audio.usb.$(TARGET_BOARD_PLATFORM) \
-    audio.hdmi.$(TARGET_BOARD_PLATFORM) \
-    audio_policy.default.so \
-    audio_configuration_files
-
 # Audio HAL
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service
 
-#Audio policy engine configuration files
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/default/policy/audio_policy_criteria.conf:vendor/etc/audio_policy_criteria.conf \
-    $(LOCAL_PATH)/audio/default/policy/audio_policy_engine_product_strategies.xml:vendor/etc/audio_policy_engine_product_strategies.xml \
-    $(LOCAL_PATH)/audio/default/policy/audio_policy_engine_configuration.xml:vendor/etc/audio_policy_engine_configuration.xml \
-    $(LOCAL_PATH)/audio/default/policy/audio_policy_engine_criteria.xml:vendor/etc/audio_policy_engine_criteria.xml \
-    $(LOCAL_PATH)/audio/default/policy/audio_policy_engine_criterion_types.xml.in:vendor/etc/audio_policy_engine_criterion_types.xml.in
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml\
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-# Vendor audio configuration files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/default/policy/audio_policy_configuration.xml:vendor/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/default/policy/a2dp_audio_policy_configuration.xml:vendor/etc/a2dp_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/default/policy/r_submix_audio_policy_configuration.xml:vendor/etc/r_submix_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/default/policy/usb_audio_policy_configuration.xml:vendor/etc/usb_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/default/policy/hdmi_audio_policy_configuration.xml:vendor/etc/hdmi_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/default/policy/audio_policy_volumes.xml:vendor/etc/audio_policy_volumes.xml \
-    $(LOCAL_PATH)/audio/default/policy/default_volume_tables.xml:vendor/etc/default_volume_tables.xml \
-    $(LOCAL_PATH)/audio/default/effect/audio_effects.xml:vendor/etc/audio_effects.xml \
-    $(LOCAL_PATH)/audio/default/mixer_paths_0.xml:vendor/etc/mixer_paths_0.xml
 
-#fallback
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/default/policy/fallback/audio_policy_configuration_generic.xml:system/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/default/policy/fallback/primary_audio_policy_configuration.xml:system/etc/primary_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/default/policy/fallback/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/default/policy/fallback/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
-    $(LOCAL_PATH)/audio/default/policy/fallback/default_volume_tables.xml:system/etc/default_volume_tables.xml
-
+PFW_CONFIGURATION_FOLDER := /vendor/etc/parameter-framework/
 
 PRODUCT_PROPERTY_OVERRIDES += audio.safemedia.bypass=true
 
-PRODUCT_PACKAGE_OVERLAYS += $(INTEL_PATH_COMMON)/audio/overlay-car-legacy
