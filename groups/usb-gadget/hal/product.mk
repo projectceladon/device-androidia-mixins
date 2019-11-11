85a6917069a7099512a8cba8b76acb66e891d1fc
saranya <saranya.gopal@intel.com>
2019-11-11 14:20:11 +0530

    mixins: Add mixins to enable USB Gadget HAL
    Tracked-On: OAM-88508
    Signed-off-by: Saranya Gopal <saranya.gopal@intel.com>

---
+++ groups/usb-gadget/hal/product.mk
@@ -0,0 +1,15 @@
+ifeq ($(TARGET_BUILD_VARIANT),user)
+# Enable Secure Debugging
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
+
+ifeq ($(BUILD_FOR_CTS_AUTOMATION),true)
+PRODUCT_COPY_FILES += $(INTEL_PATH_COMMON)/usb-gadget/adb_keys:root/adb_keys
+endif #BUILD_FOR_CTS_AUTOMATION == true
+endif #TARGET_BUILD_VARIANT == user
+
+# Add Intel adb keys for userdebug/eng builds
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_COPY_FILES += $(INTEL_PATH_COMMON)/usb-gadget/adb_keys:root/adb_keys
+endif
+
+PRODUCT_PACKAGES += android.hardware.usb@1.1-service.intel
