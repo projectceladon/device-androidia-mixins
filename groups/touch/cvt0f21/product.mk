d553d45d72267367990c2d11c81863ee75750cb5
rbabub <ramesh.babu.b@intel.com>
2019-12-18 15:06:52 +0530

    [CIV] Enable 4k Touch device
    Adding the changes to support cvt 4k display
    
    Tracked-On: OAM-88790
    Signed-off-by: rbabub <ramesh.babu.b@intel.com>

---
+++ groups/touch/cvt0f21/product.mk
@@ -0,0 +1,4 @@
+PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:vendor/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml\
+        $(INTEL_PATH_COMMON)/touch/Vendor_1ff7_Product_0f21.idc:system/usr/idc/Vendor_1ff7_Product_0f21.idc
+
