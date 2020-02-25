d2f96b0a314d745f4a01b95299244c536e4066c0
Huang Yang <yang.huang@intel.com>
2020-02-25 13:34:39 +0800

    Update mixins for Trusty
    Tracked-On:
    Signed-off-by: Huang Yang <yang.huang@intel.com>

---
+++ groups/device-specific/cic/option.spec
@@ -0,0 +1,8 @@
+[defaults]
+target = cic
+ref_target = cic
+gk_force_passthrough = false
+ota-update = false
+
+[mixinfo]
+deps = device-type
--- groups/device-specific/cic/product.mk
+++ groups/device-specific/cic/product.mk
@@ -12,5 +12,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@4.0-strongbox-service \
 
