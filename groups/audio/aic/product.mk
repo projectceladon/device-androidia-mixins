18b9e36209389d81667c8db31713479252ce30aa
akodanka <anoob.anto.kodankandath@intel.com>
2020-02-26 20:57:23 +0530

    Enable intel audio hal for cic secure
    Copying celadon's BoardConfig to aic folder.
    
    Tracked-On: OAM-89771
    Signed-off-by: akodanka <anoob.anto.kodankandath@intel.com>

--- groups/audio/aic/product.mk
+++ groups/audio/aic/product.mk
@@ -7,9 +7,24 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/audiopolicy/config/audio_policy_configuration_generic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration_re.xml \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes_re.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables_re.xml \
     frameworks/av/services/audiopolicy/config/surround_sound_configuration_5_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/surround_sound_configuration_5_0.xml \
+    $(LOCAL_PATH)/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/vendor/etc/a2dp_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audiopolicy/config/audio_policy_configuration.xml:system/vendor/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audiopolicy/config/usb_audio_policy_configuration.xml:system/vendor/etc/usb_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audiopolicy/config/hdmi_audio_policy_configuration.xml:system/vendor/etc/hdmi_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audiopolicy/config/mixer_paths_0.xml:system/vendor/etc/mixer_paths_0.xml \
+    $(LOCAL_PATH)/audiopolicy/config/default_volume_tables.xml:system/vendor/etc/default_volume_tables.xml \
+    $(LOCAL_PATH)/audiopolicy/config/audio_policy_volumes.xml:system/vendor/etc/audio_policy_volumes.xml
+
+
+# Audio Primary HAL
+PRODUCT_PACKAGES += \
+    audio.primary.$(TARGET_PRODUCT)
+
+# Extended Audio HALs
+PRODUCT_PACKAGES += \
+    audio.r_submix.default \
+    audio.usb.default \
+    audio.usb.$(TARGET_PRODUCT) \
+    audio_policy.default.so \
+    audio_configuration_files
