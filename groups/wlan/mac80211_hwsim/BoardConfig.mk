2132da0019cbda918744e64790abb9119572c055
bxu10x <bingx.xu@intel.com>
2020-03-20 13:48:30 +0800

    Remove useless code in wifi module.
    Tracked-On: OAM-90451
    Signed-off-by: bxu10x <bingx.xu@intel.com>

--- groups/wlan/mac80211_hwsim/BoardConfig.mk
+++ groups/wlan/mac80211_hwsim/BoardConfig.mk
@@ -1,6 +1,4 @@
-BOARD_WLAN_DEVICE           := emulator
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_simulated
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 WIFI_DRIVER_FW_PATH_PARAM   := "/dev/null"
 WIFI_DRIVER_FW_PATH_STA     := "/dev/null"
