5c44a34a5d037d59db027c72c39046f0f47c9dd2
ji, zhenlong z <zhenlong.z.ji@intel.com>
2020-03-23 19:42:36 +0800

    Enable selinux for cic
    Tracked-On: OAM-89780
    Signed-off-by: ji, zhenlong z <zhenlong.z.ji@intel.com>

--- groups/bluetooth/cic/BoardConfig.mk
+++ groups/bluetooth/cic/BoardConfig.mk
@@ -2,4 +2,5 @@ BOARD_HAVE_BLUETOOTH_INTEL_ICNV := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/intel/project-celadon/$(TARGET_PRODUCT)/bluetooth/tablet/
 DEVICE_PACKAGE_OVERLAYS += device/intel/project-celadon/$(TARGET_PRODUCT)/bluetooth/tablet/overlay
 
-BOARD_SEPOLICY_DIRS += device/intel/sepolicy/bluetooth
+BOARD_SEPOLICY_DIRS += $(INTEL_PATH_DEVICE_CIC)/sepolicy/bluetooth
+BOARD_SEPOLICY_DIRS += $(INTEL_PATH_DEVICE_CIC)/sepolicy/bluetooth/common
