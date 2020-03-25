927c92399e6a9efaf7ad6fc27fb6decd54da9db6
Yan, Shaopu <shaopu.yan@intel.com>
2020-03-25 10:45:04 +0800

    add dm-verity support for cic
    Signed-off-by: Yan, Shaopu <shaopu.yan@intel.com>

--- groups/device-specific/cic_dev/product.mk
+++ groups/device-specific/cic_dev/product.mk
@@ -16,5 +16,14 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service \
 
+ifeq ($(TARGET_DM_VERITY_SUPPORT), true)
+PRODUCT_PACKAGES += \
+    build_verity_img.py \
+    cic_veritysetup
+
+INSTALLED_VERITY_METADATA_IMG := out/target/product/$(TARGET_PRODUCT)/verity_metadata
+INSTALLED_VERITY_SETUP_TOOL := out/host/linux-x86/bin/cic_veritysetup
+endif
+
 PRODUCT_PROPERTY_OVERRIDES += \
     service.adb.tcp.port=5555
