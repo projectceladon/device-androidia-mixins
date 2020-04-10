ee0f064a3a3c84f28439dc78f02f2eaf440bc613
sgnanase <sundar.gnanasekaran@intel.com>
2020-04-10 08:12:52 +0530

    Disable BT by default to avoid crash. Use host BT
    Tracked-On: OAM-90652
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- groups/bluetooth/cic/product.mk
+++ groups/bluetooth/cic/product.mk
@@ -8,7 +8,7 @@ PRODUCT_PACKAGES += \
     hcitool
 endif
 
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
