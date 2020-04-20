22c3cd8444e093738b5d5f4dc3dc61a7d4c5a0a9
Yan, Shaopu <shaopu.yan@intel.com>
2020-04-20 16:20:55 +0800

    add dm-verity support for cic
    1. verity_metadata is the metadata images of the verity infomations of the system.img
    2. the cic_veritysetup is a customer wrapper tool for veritysetup which is used to
    parse the verity_metadata image in aic-manager when mount the system.img for android container
    
    Tracked-On: OAM-89713
    Signed-off-by: Yan, Shaopu <shaopu.yan@intel.com>

--- groups/device-specific/cic/cic.mk
+++ groups/device-specific/cic/cic.mk
@@ -27,6 +27,11 @@ PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_AAPT_CONFIG := normal large xlarge mdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 
+{{#dm_verity}}
+TARGET_LOOP_MOUNT_SYSTEM_IMAGES := true
+TARGET_DM_VERITY_SUPPORT := true
+{{/dm_verity}}
+
 INTEL_PATH_DEVICE_CIC := device/intel/project-celadon/$(TARGET_PRODUCT)
 INTEL_PATH_KERNEL_MODULES_CIC := kernel/modules/cic
 INTEL_PATH_VENDOR_CIC := vendor/intel/cic
