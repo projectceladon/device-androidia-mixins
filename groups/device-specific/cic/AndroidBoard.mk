7beeea29b7a494e565b45bc338b179a3605d176b
rnaidu <ramya.v.naidu@intel.com>
2020-05-13 09:36:35 +0530

    Creating new target cic_dev for nonsecure
    and cic target for secure cic builds.
    Also aligning mixins to one mixin group
    Removing symlink from cic target.
    
    Tracked-On: OAM-90809
    Signed-off-by: rnaidu <ramya.v.naidu@intel.com>

--- groups/device-specific/cic/AndroidBoard.mk
+++ groups/device-specific/cic/AndroidBoard.mk
@@ -1,1 +1,92 @@
-../cic_dev/AndroidBoard.mk
+.PHONY: multidroid
+multidroid: droid addon kf4cic-$(TARGET_BUILD_VARIANT)
+	@echo Make multidroid image...
+	$(hide) rm -rf $(PRODUCT_OUT)/docker
+	$(hide) mkdir -p $(PRODUCT_OUT)/docker/android/root
+	$(hide) cp -r $(TOP)/$(INTEL_PATH_VENDOR_CIC)/host/docker/aic-manager $(PRODUCT_OUT)/docker
+	$(hide) mkdir -p $(PRODUCT_OUT)/docker/aic-manager/data/ashmem
+	$(hide) cp -r $(TOP)/kernel/modules/cic/ashmem/* $(PRODUCT_OUT)/docker/aic-manager/data/ashmem/
+	$(hide) mkdir -p $(PRODUCT_OUT)/docker/aic-manager/data/binder
+	$(hide) cp -r $(TOP)/kernel/modules/cic/binder/* $(PRODUCT_OUT)/docker/aic-manager/data/binder/
+	$(hide) cp -r $(TOP)/$(INTEL_PATH_VENDOR_CIC)/host/docker/android $(PRODUCT_OUT)/docker
+	$(hide) cp -r $(TOP)/$(INTEL_PATH_VENDOR_CIC)/host/docker/update $(PRODUCT_OUT)/docker
+	$(hide) cp $(TOP)/$(INTEL_PATH_VENDOR_CIC)/host/docker/scripts/aic $(PRODUCT_OUT)
+	$(hide) cp -r $(PRODUCT_OUT)/efi/kf4cic.efi $(PRODUCT_OUT)/kf4cic.efi
+ifneq ($(TARGET_LOOP_MOUNT_SYSTEM_IMAGES), true)
+	$(hide) cp -r $(PRODUCT_OUT)/system $(PRODUCT_OUT)/docker/android/root
+	$(hide) cp -r $(PRODUCT_OUT)/root/* $(PRODUCT_OUT)/docker/android/root
+	$(hide) rm -f $(PRODUCT_OUT)/docker/android/root/etc
+	$(hide) cp -r $(PRODUCT_OUT)/system/etc $(PRODUCT_OUT)/docker/android/root
+	$(hide) chmod -R g-w $(PRODUCT_OUT)/docker/android/root
+else
+	$(hide) zcat $(PRODUCT_OUT)/ramdisk.img | (cd $(PRODUCT_OUT)/docker/android/root && cpio -idm)
+	$(hide) rm -rf $(PRODUCT_OUT)/docker/android/root/etc
+	$(hide) cp -r $(PRODUCT_OUT)/system/etc $(PRODUCT_OUT)/docker/android/root
+	$(hide) rm -rf $(PRODUCT_OUT)/docker/aic-manager/images
+	$(hide) mkdir -p $(PRODUCT_OUT)/docker/aic-manager/images
+ifeq ($(TARGET_DM_VERITY_SUPPORT), true)
+	$(hide) python $(HOST_OUT_EXECUTABLES)/build_verity_img.py $(PRODUCT_OUT)/system.img $(PRODUCT_OUT)/verity_metadata
+endif
+	$(hide) ln -t $(PRODUCT_OUT)/docker/aic-manager/images $(PRODUCT_OUT)/system.img
+endif
+
+TARGET_AIC_FILE_NAME := $(TARGET_PRODUCT)-aic-$(BUILD_NUMBER).tar.gz
+
+.PHONY: addon
+addon:
+ifneq (,$(filter cic cic_dev,$(TARGET_PRODUCT)))
+	@echo Make additional release binaries/files...
+	$(hide) rm -rf $(PRODUCT_OUT)/cfc $(PRODUCT_OUT)/pre-requisites $(PRODUCT_OUT)/sof_audio  $(PRODUCT_OUT)/README-CIC  $(PRODUCT_OUT)/ia_hwc $(PRODUCT_OUT)/setup-aic $(PRODUCT_OUT)/INFO
+	$(hide) cp -r $(TOP)/device/intel/project-celadon/$(TARGET_PRODUCT)/addon/* $(TOP)/vendor/intel/cic/host/cfc $(TOP)/vendor/intel/cic/target/graphics/ia_hwc $(PRODUCT_OUT)/.
+	$(hide) cp -r $(TOP)/vendor/intel/cic/README $(PRODUCT_OUT)/INFO
+	$(hide) cp $(TOP)/device/intel/project-celadon/$(TARGET_PRODUCT)/addon/cic.sh $(PRODUCT_OUT)
+else
+	@echo Nothing todo
+endif
+
+ifeq ($(BOARD_AVB_ENABLE),true)
+VBMETA_IMG := vbmeta.img
+else
+VBMETA_IMG :=
+endif
+
+.PHONY: aic
+aic: .KATI_NINJA_POOL := console
+aic: multidroid tosimage
+	@echo Make AIC docker images...
+ifneq ($(TARGET_LOOP_MOUNT_SYSTEM_IMAGES), true)
+	$(HOST_OUT_EXECUTABLES)/aic-build -b $(BUILD_NUMBER)
+else
+	BUILD_VARIANT=loop_mount $(HOST_OUT_EXECUTABLES)/aic-build -b $(BUILD_NUMBER)
+endif
+ifneq (,$(filter cic cic_dev,$(TARGET_PRODUCT)))
+	tar cvzf $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) -C $(PRODUCT_OUT) aic android.tar.gz aic-manager.tar.gz cfc ia_hwc pre-requisites sof_audio README-CIC INFO cic.sh setup-aic tos.img $(VBMETA_IMG) kf4cic.efi verity_metadata -C docker update
+	@echo Make debian binaries...
+	$(hide) (rm -rf $(PRODUCT_OUT)/cic && mkdir -p $(PRODUCT_OUT)/cic/opt/cic && mkdir -p $(PRODUCT_OUT)/cic/etc/profile.d)
+	$(hide) (cd $(PRODUCT_OUT)/cic/opt/cic && tar xvf ../../../$(TARGET_AIC_FILE_NAME) aic android.tar.gz aic-manager.tar.gz INFO cic.sh cfc update)
+	$(hide) mkdir -p $(PRODUCT_OUT)/cic/DEBIAN
+	$(hide) cp -r device/intel/project-celadon/$(TARGET_PRODUCT)/addon/debian/* $(PRODUCT_OUT)/cic/DEBIAN/.
+	$(hide) cp -r device/intel/project-celadon/$(TARGET_PRODUCT)/addon/pre-requisites/create_pasocket.sh $(PRODUCT_OUT)/cic/etc/profile.d
+	$(hide) dpkg -x $(PRODUCT_OUT)/cfc/cfc_0.1.0_x64.deb $(PRODUCT_OUT)/cic/.
+	$(hide) (cd $(PRODUCT_OUT)/ && dpkg-deb --build cic/)
+else
+	tar cvzf $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) -C $(PRODUCT_OUT) aic android.tar.gz aic-manager.tar.gz -C docker update
+endif
+
+.PHONY: cic
+cic: aic
+
+.PHONY: publish_ci
+publish_ci: aic
+	@echo Publish CI AIC docker images...
+	$(hide) mkdir -p $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
+	$(hide) cp $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
+
+# Following 1A CI practice, "publish" is used by buildbot for "latest", "release", etc. Without this
+# target, the build will fail on related buildbot.
+# Currently, the "publish" and "publish_ci" are the same. But they may be different in the future.
+.PHONY: publish
+publish: aic
+	@echo Publish AIC docker images...
+	$(hide) mkdir -p $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
+	$(hide) cp $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
