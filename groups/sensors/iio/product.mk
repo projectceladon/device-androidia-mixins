d7d91ebf7313f5c474485d8808050dbd807221a5
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2020-08-19 11:23:35 +0530

    Add iio sensor vendor hal
    - Added iio sensor vendor hal to manifest.xml
    - Added mixins for iio sensor
    - Updated android-removed-permissions.xml
    
    Tracked-On: OAM-92192
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
    Signed-off-by: Jaikrishna Nemallapudi <Nemallapudi.Jaikrishna@intel.com>

---
+++ groups/sensors/iio/product.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/vendor/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/vendor/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/vendor/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/vendor/etc/permissions/android.hardware.sensor.compass.xml
+
+# Sensors HAL modules
+PRODUCT_PACKAGES += \
+    sensors.$(TARGET_PRODUCT) \
+    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-impl
