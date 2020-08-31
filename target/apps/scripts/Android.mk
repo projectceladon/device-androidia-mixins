4525e2e7e76e64c67c81862c1e7f41f294cfb45c
fanzha3x <fanx.zhang@intel.com>
2020-08-31 14:22:44 +0800

    Remove needless code to avoid using root permissions
    Tracked-On: OAM-92272
    Signed-off-by: fanzha3x <fanx.zhang@intel.com>

--- target/apps/scripts/Android.mk
+++ target/apps/scripts/Android.mk
@@ -7,17 +7,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := auto_start_apk.sh
-LOCAL_SRC_FILES := auto_start_apk.sh
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_MODULE_SUFFIX :=
-LOCAL_BUILT_MODULE_STEM := $(notdir $(LOCAL_SRC_FILES))
-include $(BUILD_PREBUILT)
-
-
-
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := delRedundantInput.sh
 LOCAL_SRC_FILES := delRedundantInput.sh
 LOCAL_MODULE_CLASS := EXECUTABLES
--- target/apps/scripts/auto_start_apk.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-
-customize_script="/data/local/tmp/customize_script.sh"
-if [ -f ${customize_script} ];
-then
-	${customize_script}
-fi
