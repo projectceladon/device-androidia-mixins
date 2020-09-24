b4dc60414bf2afc45b306254151397692e19c710
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2020-09-24 16:21:21 +0530

    iwlwifi: cleanup iwlwifi mixins
    Changes include:
    - Remove not used config from iwlwifi mixins
    - Added correct user and group for dhcpcd
    
    Tracked-On: OAM-93073
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- groups/wlan/iwlwifi/product.mk
+++ groups/wlan/iwlwifi/product.mk
@@ -8,20 +8,7 @@ PRODUCT_PACKAGES += \
     iw
 
 PRODUCT_PACKAGES += \
-  android.hardware.wifi@1.0-service
-
-# FW and PNVM
-PRODUCT_PACKAGES += \
-{{#firmware}}
-    {{firmware}} \
-{{/firmware}}
-{{#nvm}}
-    iwl-nvm
-{{/nvm}}
-
-# iwlwifi USC
-PRODUCT_PACKAGES += \
-    wifi_intel_usc
+    android.hardware.wifi@1.0-service
 
 #copy iwlwifi wpa config files
 PRODUCT_COPY_FILES += \
@@ -30,25 +17,5 @@ PRODUCT_COPY_FILES += \
         $(INTEL_PATH_COMMON)/wlan/iwlwifi/p2p_supplicant_overlay.conf:vendor/etc/wifi/p2p_supplicant_overlay.conf \
         frameworks/native/data/etc/android.hardware.wifi.xml:vendor/etc/permissions/android.hardware.wifi.xml \
         frameworks/native/data/etc/android.hardware.wifi.direct.xml:vendor/etc/permissions/android.hardware.wifi.direct.xml
-
-PRODUCT_COPY_FILES += \
-    vendor/linux/firmware/iwlwifi-9260-th-b0-jf-b0-43.ucode:$(TARGET_COPY_OUT_VENDOR)/firmware/iwlwifi-9260-th-b0-jf-b0-43.ucode \
-    vendor/linux/firmware/iwlwifi-3168-29.ucode:$(TARGET_COPY_OUT_VENDOR)/firmware/iwlwifi-3168-29.ucode \
-    vendor/linux/firmware/iwlwifi-8265-36.ucode:$(TARGET_COPY_OUT_VENDOR)/firmware/iwlwifi-8265-36.ucode
-
-{{#gpp}}
-# Add Manufacturing tool
-PRODUCT_PACKAGES += \
-    wlan_intel_restore.sh
-{{/gpp}}
-
-{{#softap_dualband_allow}}
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.wifi.softap_dualband_allow=true
-{{/softap_dualband_allow}}
-{{^softap_dualband_allow}}
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.wifi.softap_dualband_allow=false
-{{/softap_dualband_allow}}
 
 PRODUCT_PACKAGE_OVERLAYS += $(INTEL_PATH_COMMON)/wlan/overlay-disable_keepalive_offload
