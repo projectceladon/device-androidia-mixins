64c3e60c9717369cc05567390da7b67512955ceb
svenate <salini.venate@intel.com>
2020-09-28 10:14:36 +0530

    To fix Chrome crash

--- groups/default-drm/true/product.mk
+++ groups/default-drm/true/product.mk
@@ -1,5 +1,6 @@
 #only enable default drm service
 PRODUCT_PACKAGES += android.hardware.drm@1.0-service \
                     android.hardware.drm@1.0-impl \
-                    android.hardware.drm@1.2-service.clearkey
+                    android.hardware.drm@1.2-service.clearkey \
+                    android.hardware.drm@1.3-service.clearkey
 
