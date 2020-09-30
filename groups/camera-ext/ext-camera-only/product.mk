1e30f57541f2d27968e13cc1dfa196148a48f36f
gkdeepa <g.k.deepa@intel.com>
2020-09-30 14:32:54 +0530

    adding VHAL support for camera
    Tracked-On:

--- groups/camera-ext/ext-camera-only/product.mk
+++ groups/camera-ext/ext-camera-only/product.mk
@@ -1,10 +1,12 @@
 # Camera: Device-specific configuration files. Supports only External USB camera, no CSI support
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.external.xml:vendor/etc/permissions/android.hardware.camera.external.xml \
-    $(LOCAL_PATH)/{{_extra_dir}}/external_camera_config.xml:vendor/etc/external_camera_config.xml
+    $(LOCAL_PATH)/{{_extra_dir}}/external_camera_config.xml:vendor/etc/external_camera_config.xml\
+    $(LOCAL_PATH)/{{_extra_dir}}/camera.celadon.so:vendor/lib/hw/camera.celadon.so
 
 # External camera service
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-external-service \
+                    android.hardware.camera.provider@2.4-service \
                     android.hardware.camera.provider@2.4-impl
 
 # Only include test apps in eng or userdebug builds.
