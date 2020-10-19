0331874a27297a5aa684b743575225d68b4da562
Tanuj Tekriwal <tanuj.tekriwal@intel.com>
2020-10-19 10:22:32 +0530

    Revert "Adding mixin for mainline modules"
    This reverts commit cf68d1f6ba935a29e6ab40b411aba61473072cdf.

--- groups/mainline-mod/true/product.mk
+++
@@ -1,5 +0,0 @@
-# Enable updating of APEXes
- $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Enable userspace reboot
- $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
