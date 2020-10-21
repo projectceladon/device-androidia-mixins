bd4970eb023514398b4947b28e1637adcee5ac93
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2020-10-21 11:27:34 +0530

    Add packages and files needed for openvino 2020.4
    Tracked-On: OAM-94515
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- groups/neuralnetworks/true/product.mk
+++ groups/neuralnetworks/true/product.mk
@@ -2,17 +2,12 @@
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.2-generic-service \
     android.hardware.neuralnetworks@1.2-generic-impl \
-    android.hardware.neuralnetworks@1.2-service-gpgpu
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/{{_extra_dir}}/plugins.xml:vendor/etc/openvino/plugins.xml \
+
 
 PRODUCT_PACKAGES += \
-    libinference_engine
-
-PRODUCT_PACKAGES += \
-    libMKLDNNPlugin\
-    libmkldnn \
-    libmkldnn \
-    libclDNNPlugin \
-    libclDNN64
-
-PRODUCT_PACKAGES += \
-    graphtest_cpu
+    libMKLDNNPlugin \
+    libinference_engine_preproc \
+    libinference_engine_ir_reader
