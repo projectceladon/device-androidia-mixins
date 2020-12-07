158be9484f2df2526ccb9595ca5a94a46a7a269d
ahs <amrita.h.s@intel.com>
2020-12-07 14:50:20 +0530

    Revert "Add perf variants of libraries into product packages"
    This reverts commit f8efdb6a84cdc605e8c48fcea10c3b54cbdadcac.

--- groups/avx/auto/product.mk
+++ groups/avx/auto/product.mk
@@ -1,8 +1,1 @@
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/extra_files/avx/checkavx.sh:vendor/bin/checkavx.sh
-
-PRODUCT_PACKAGES += libaudioprocessing_avx2 \
-		    libneuralnetworks_avx2 \
-		    libRSCpuRef_avx2 \
-		    libart_avx2.com.android.art.debug \
-		    libart_avx2.com.android.art.release \
-		    libartd_avx2.com.android.art.debug
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/extra_files/avx/checkavx.sh:vendor/bin/checkavx.sh 
