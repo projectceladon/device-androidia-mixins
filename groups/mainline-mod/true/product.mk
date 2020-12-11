dcdaacf458d747967ad49cd9ad85daf48a52d7d1
svenate <salini.venate@intel.com>
2020-12-12 00:09:11 +0530

    Revert "Disable Mainline module dependency changes"
    Enabling the mainline module changes again as the
    dependent issue related to adb reboot is resolved
    
    This reverts commit f3a60e308cf7c4527a248e91bca710422e0df312.
    
    Tracked-On: OAM-95459
    Signed-off-by: svenate <salini.venate@intel.com>

---
+++ groups/mainline-mod/true/product.mk
@@ -0,0 +1,6 @@
+# Enable updating of APEXes
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Enable userspace reboot
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
+
