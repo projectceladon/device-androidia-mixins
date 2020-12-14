246c8023c72f47ad1eff04a7ae3f583ebb7520e5
Shwetha B <shwetha.b@intel.com>
2020-12-14 10:06:49 +0530

    Mixin changes for using Power HAL AIDL
    Removing the entry for power from manifest.xml and
    framework_manifest.xml because it will be added to
    VINTF manifest in vendor-intel-hardware-interfaces
    
    Include the sepolicy for Power in Boardconfig.mk
    
    Tracked-On: OAM-95367
    Signed-off-by: Shwetha B <shwetha.b@intel.com>

--- groups/power/true/product.mk
+++ groups/power/true/product.mk
@@ -1,6 +1,4 @@
 # Power HAL
 PRODUCT_PACKAGES += power.$(TARGET_BOARD_PLATFORM) \
-                    android.hardware.power@1.0-service \
-                    android.hardware.power@1.0-impl \
-                    power_hal_helper
-
+                    android.hardware.power-service.intel \
+		    power_hal_helper
