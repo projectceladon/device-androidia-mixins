b73cc3d51645d03647e4f4a07b9e5bffbc68b004
Shaofeng Tang <shaofeng.tang@intel.com>
2021-03-03 14:36:34 +0800

    Enable Remote HWC and remove HDCPD
    Signed-off-by: Shaofeng Tang <shaofeng.tang@intel.com>

--- groups/device-specific/caas/product.mk
+++ groups/device-specific/caas/product.mk
@@ -27,7 +27,8 @@ PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl \
                     android.hardware.graphics.mapper@4.0-impl.minigbm \
                     android.hardware.graphics.allocator@4.0-service.minigbm \
                     android.hardware.renderscript@1.0-impl \
-                    android.hardware.graphics.composer@2.4-service
+		    android.hardware.graphics.composer@2.3-impl \
+                    android.hardware.graphics.composer@2.3-service
 
 
 PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
