f56bebd85a5a3eb18da74fb3f8cf6a8026cbb326
Kothapeta, BikshapathiX <bikshapathix.kothapeta@intel.com>
2021-04-08 15:34:38 +0530

    drm: remove drm 1.0 packages
    Even when DRM version is set to 1.3, drm 1.0 service is running
    which leads to CTS, VTS failure.
    
    Fix the issue by removing drm 1.0 packages.
    
    Tracked-On: OAM-96821
    Signed-off-by: Kothapeta, BikshapathiX <bikshapathix.kothapeta@intel.com>

--- groups/default-drm/true/product.mk
+++ groups/default-drm/true/product.mk
@@ -1,5 +1,3 @@
 #only enable default drm service
-PRODUCT_PACKAGES += android.hardware.drm@1.0-service \
-                    android.hardware.drm@1.0-impl \
-                    android.hardware.drm@1.3-service.clearkey
+PRODUCT_PACKAGES += android.hardware.drm@1.3-service.clearkey
 
