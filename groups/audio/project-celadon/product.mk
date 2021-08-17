857fe5140da655f9b88a3eabc27827a54df6b028
pmandri <padmashree.mandri@intel.com>
2021-08-17 14:42:03 +0530

    Upgrade Audio HAL from 6 to 7
    Updated files: product.mk, framework_manifest.xml and manifest.xml
    Tracked-On: OAM-97778
    Signed-off-by: pmandri <padmashree.mandri@intel.com>

--- groups/audio/project-celadon/product.mk
+++ groups/audio/project-celadon/product.mk
@@ -30,8 +30,8 @@ PRODUCT_PACKAGES += \
 
 # Audio HAL
 PRODUCT_PACKAGES += \
-    android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@7.0-impl \
     android.hardware.audio@2.0-service
 
 #Audio policy engine configuration files
