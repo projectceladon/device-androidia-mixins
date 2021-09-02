2f4f5919a079c134501c34759ac6a954ab972fec
ahs <amrita.h.s@intel.com>
2021-09-02 15:23:56 +0530

    Include ClipboardAgent system app to caas_cfc
    This is to support clipboard copy/paste feature
    for caas_cfc target.
    
    Tracked-On: OAM-99249
    Signed-off-by: ahs <amrita.h.s@intel.com>

--- groups/device-specific/caas_cfc/product.mk
+++ groups/device-specific/caas_cfc/product.mk
@@ -22,6 +22,8 @@ PRODUCT_PACKAGES += \
     ServiceAgent \
     pm_agent_client
 
+PRODUCT_PACKAGES += ClipboardAgent
+
 PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl \
                     android.hardware.keymaster@3.0-service \
                     android.hardware.usb@1.0-impl \
