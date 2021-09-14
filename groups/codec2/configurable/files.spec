692b99d7569567745a653744a73b67ed126013a1
Yang, Dong <dong.yang@intel.com>
2021-09-14 08:53:36 +0800

    Enable virtio-video driver on Android T
    Copy library into system folder
    Deploy V4L2 dependency so files into system folder
    Enable H265 in V4L2
    Uncomment the HEVX/H265 setting in V4L2 configuration.
    Update the library path for Android T
    Enable VirtioVideo on Kernel
    work on T with kernel 5.15/lts2021-chromium
    Add the Default policy for codec2.0 service
    add 1.1 of the default policy for codec2.0 service
    
    Update the path of V4L2-codecs
    
    Tracked-On: OAM-104252
    Signed-off-by: Yang, Dong <dong.yang@intel.com>
    Signed-off-by: Shaofeng Tang <shaofeng.tang@intel.com>

---
+++ groups/codec2/configurable/files.spec
@@ -0,0 +1,5 @@
+[extrafiles]
+media_codecs_c2_v4l2.xml: "Specific configuration for audio and video codecs for v4l2"
+media_codecs_performance_c2_v4l2.xml: "Media codecs performance file for v4l2"
+codec2.vendor.ext.policy: "V4L2 Codec2 policy"
+android.hardware.media.c2@1.1-default-seccomp_policy: "Default policy for codec2.0 service"
--- groups/media/auto/AndroidBoard.mk
+++ groups/media/auto/AndroidBoard.mk
@@ -1,2 +1,10 @@
 AUTO_IN += $(TARGET_DEVICE_DIR)/{{_extra_dir}}/auto_hal.in
 
+$(PRODUCT_OUT)/system.img: codec2_copy_modules
+codec2_copy_modules : android.hardware.media.c2@1.0-service-v4l2 libv4l2_codec2_common libc2plugin_store
+	cp $(PRODUCT_OUT)/vendor/lib/libc2plugin_store.so $(PRODUCT_OUT)/system/lib/libc2plugin_store.so
+	cp $(PRODUCT_OUT)/vendor/lib/libstagefright_bufferpool@1.0.so $(PRODUCT_OUT)/system/lib/libstagefright_bufferpool@1.0.so
+	cp $(PRODUCT_OUT)/vendor/lib/android.hardware.media.bufferpool@1.0.so $(PRODUCT_OUT)/system/lib/android.hardware.media.bufferpool@1.0.so
+	cp $(PRODUCT_OUT)/vendor/lib64/libc2plugin_store.so $(PRODUCT_OUT)/system/lib64/libc2plugin_store.so
+	cp $(PRODUCT_OUT)/vendor/lib64/libstagefright_bufferpool@1.0.so $(PRODUCT_OUT)/system/lib64/libstagefright_bufferpool@1.0.so
+	cp $(PRODUCT_OUT)/vendor/lib64/android.hardware.media.bufferpool@1.0.so $(PRODUCT_OUT)/system/lib64/android.hardware.media.bufferpool@1.0.so
