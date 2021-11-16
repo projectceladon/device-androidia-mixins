0604da3dbe712b15b6dd5ac30d43b0b4c3d3b8ca
yuxincui <yuxin.cui@intel.com>
2021-11-16 14:11:33 +0800

    Enable com.android.remoteprovisioner service
    It enables com.android.remoteprovisioner service.
    
    Tracked-On: OAM-100082
    Signed-off-by: yuxincui <yuxin.cui@intel.com>

--- groups/trusty/true/product.mk
+++ groups/trusty/true/product.mk
@@ -8,12 +8,15 @@ PRODUCT_PACKAGES += \
 	android.hardware.gatekeeper@1.0-service.trusty \
 	android.hardware.security.keymint-service.trusty \
 	keybox_provisioning \
+	RemoteProvisioner
 
 PRODUCT_PACKAGES_DEBUG += \
 	intel-secure-storage-unit-test \
 	gatekeeper-unit-tests \
 	libscrypt_static \
 	scrypt_test \
+	RemoteProvisionerUnitTests \
+	libkeymint_remote_prov_support_test
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.hardware.gatekeeper=trusty \
