c719e42158977d40d9877d29d719a8f14b1f3291
rajucm <raju.mallikarjun.chegaraddi@intel.com>
2021-11-24 13:11:03 +0530

    sensorhal-2.0: mixin changes..
    sensor-2.0 - Adding support
    sensor-1.0 - Remove support
    
    Tracked-On: OAM-99160
    Signed-off-by: rajucm <raju.mallikarjun.chegaraddi@intel.com>

--- groups/sensors/mediation/product.mk
+++ groups/sensors/mediation/product.mk
@@ -2,13 +2,8 @@ ifeq ($(TARGET_BOARD_PLATFORM),)
     $(error Please define TARGET_BOARD_PLATFORM in product-level Makefile)
 endif
 
-# Sensors HAL modules
 PRODUCT_PACKAGES += \
-        sensors.$(TARGET_BOARD_PLATFORM)
-
-PRODUCT_PACKAGES += \
-        android.hardware.sensors@1.0-service \
-        android.hardware.sensors@1.0-impl
+        android.hardware.sensors@2.0-service.intel
 
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:vendor/etc/permissions/android.hardware.sensor.ambient_temperature.xml \
