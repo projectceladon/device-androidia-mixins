57f42a130e899cb19d7ebd54dc8c35b1bf565547
Kishan Mochi <kishan.mochi@intel.com>
2021-11-30 21:47:40 +0530

    Enable zram swap for device having <= 4G ram
    zram swap to increase available memory when device is
    under memory pressure.
    
    Tracked-On: OAM-100254
    Signed-off-by: Kishan Mochi <kishan.mochi@intel.com>

--- groups/swap/zram_auto/check_lowmem.sh
+++ groups/swap/zram_auto/check_lowmem.sh
@@ -1,13 +1,13 @@
 #!/vendor/bin/sh
 
-# 2G size in kB
-SIZE_2G=2097152
+# 4G size in kB
+SIZE_4G=4194304
 
 mem_size=`cat /proc/meminfo | grep MemTotal | tr -s ' ' | cut -d ' ' -f 2`
 
-if [ "$mem_size" -le "$SIZE_2G" ]
+if [ "$mem_size" -le "$SIZE_4G" ]
 then
-    setprop vendor.low_ram 1
+    setprop vendor.le_4g_ram 1
 else
-    setprop vendor.low_ram 0
+    setprop vendor.le_4g_ram 0
 fi
