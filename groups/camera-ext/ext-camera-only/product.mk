0db5eae863f74048382f5b83e766b2d0de166ee0
gkdeepa <g.k.deepa@intel.com>
2021-12-13 18:43:26 +0530

    Revert "Include VHAL for camera"
    This reverts commit 52402f3f9feb24ff5ab7fc7cc4941c0d9f15eda7.

--- groups/camera-ext/ext-camera-only/product.mk
+++ groups/camera-ext/ext-camera-only/product.mk
@@ -5,22 +5,9 @@ PRODUCT_COPY_FILES += \
 
 # External camera service
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-external-service \
-                    android.hardware.camera.provider@2.4-service_64 \
                     android.hardware.camera.provider@2.4-impl
-#VHAL camera
-PRODUCT_PACKAGES += camera.$(TARGET_BOARD_PLATFORM) \
-                    camera.$(TARGET_BOARD_PLATFORM).jpeg
 
-PRODUCT_PROPERTY_OVERRIDES += ro.vendor.remote.sf.fake_camera ="both" \
-                              ro.vendor.camera.in_frame_format.h264=false \
-                              ro.vendor.camera.in_frame_format.i420=true \
-                              ro.vendor.camera.decode.vaapi=false \
-                              ro.vendor.remote.sf.back_camera_hal= \
-                              ro.vendor.remote.sf.front_camera_hal= \
-                              ro.vendor.camera.transference="VSOCK" \
-                              vendor.camera.external="VHAL"
-#removing not required apps
 # Only include test apps in eng or userdebug builds.
-#PRODUCT_PACKAGES_DEBUG += TestingCamera
+PRODUCT_PACKAGES_DEBUG += TestingCamera
 
-#PRODUCT_PACKAGES += MultiCameraApp
+PRODUCT_PACKAGES += MultiCameraApp
