c10683791d0df9f8a074f96c2ac1f81f20ac7fc4
Shaofeng Tang <shaofeng.tang@intel.com>
2021-12-16 15:25:45 +0800

    Copy library into system folder
    Deploy V4L2 dependency so files into system folder
    
    Tracked-On: OAM-99370
    Signed-off-by: Shaofeng Tang <shaofeng.tang@intel.com>

--- groups/media/auto/AndroidBoard.mk
+++ groups/media/auto/AndroidBoard.mk
@@ -1,2 +1,13 @@
 AUTO_IN += $(TARGET_DEVICE_DIR)/{{_extra_dir}}/auto_hal.in
 
+#libv4l2_codec2_store
+
+$(PRODUCT_OUT)/system.img: codec2_copy_modules
+codec2_copy_modules : android.hardware.media.c2@1.0-service-v4l2 libv4l2_codec2_common libc2plugin_store
+	cp $(PRODUCT_OUT)/vendor/lib/libc2plugin_store.so $(PRODUCT_OUT)/system/lib/libc2plugin_store.so
+	cp $(PRODUCT_OUT)/vendor/lib/libstagefright_bufferpool@1.0.so $(PRODUCT_OUT)/system/lib/libstagefright_bufferpool@1.0.so
+	cp $(PRODUCT_OUT)/apex/com.android.vndk.v30/lib/android.hardware.media.bufferpool@1.0.so $(PRODUCT_OUT)/system/lib/android.hardware.media.bufferpool@1.0.so
+	cp $(PRODUCT_OUT)/vendor/lib64/libc2plugin_store.so $(PRODUCT_OUT)/system/lib64/libc2plugin_store.so
+	cp $(PRODUCT_OUT)/vendor/lib64/libstagefright_bufferpool@1.0.so $(PRODUCT_OUT)/system/lib64/libstagefright_bufferpool@1.0.so
+	cp $(PRODUCT_OUT)/apex/com.android.vndk.v30/lib64/android.hardware.media.bufferpool@1.0.so $(PRODUCT_OUT)/system/lib64/android.hardware.media.bufferpool@1.0.so
+
