3823f0ac1bfbf70c66a91396a1ff452a546f6f78
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2022-01-02 15:38:25 +0530

    kernel: Update lts2020-yocto to 5.10.100
    - Updated kernel config files for lts2020-yocto 5.10.100
    kernel
    - Updated kernel command line to disable local machine check exception
    and WAITPKG feature to address civ boot issue in ADL P.
    - Set enable_guc and disable_display kernel command line
    parameters.
    
    Tracked-On: OAM-101557
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
    Signed-off-by: Kalyan Alle <kalyan.alle@intel.com>

--- groups/kernel/BoardConfig.mk
+++ groups/kernel/BoardConfig.mk
@@ -65,5 +65,11 @@ BOARD_KERNEL_CMDLINE += \
       snd-intel-dspcfg.dsp_driver=1
 endif
 
+ifeq ($(BASE_LTS2020_YOCTO_KERNEL), true)
+BOARD_KERNEL_CMDLINE += \
+      clearcpuid=517 \
+      mce=no_lmce
+endif
+
 BOARD_SEPOLICY_M4DEFS += module_kernel=true
 BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/kernel
