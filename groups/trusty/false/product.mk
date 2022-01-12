1f0ab1f3e814eb67a5fb194d1dfa1e23c660b859
yuxincui <yuxin.cui@intel.com>
2022-01-12 16:33:57 +0800

    Enable keymaster 3.0
    It will use different service binaries based on whether trusty
    is enabled or disabled:
    w/ trusty:
    android.hardware.keymaster@3.0-service.trusty
    w/o Trusty:
    android.hardware.keymaster@3.0-service
    
    Tracked-On: OAM-100659
    Signed-off-by: yuxincui <yuxin.cui@intel.com>

--- groups/trusty/false/product.mk
+++ groups/trusty/false/product.mk
@@ -1,2 +1,3 @@
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-service.software
+    android.hardware.gatekeeper@1.0-service.software \
+    android.hardware.keymaster@3.0-service
--- groups/trusty/true/option.spec
+++ groups/trusty/true/option.spec
@@ -2,5 +2,4 @@
 lk_project       = sand-x86-64
 enable_hw_sec    = true
 enable_storage_proxyd = true
-keymaster_version   = 2
 ref_target =
