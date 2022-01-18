fb83ae41359976b92eda7c071b9816268a60f60a
shivasku82 <shiva.kumara.rudrappa@intel.com>
2022-01-18 21:10:14 +0530

    Picsart application not found in google play on CIV
    Picsart application need auto focus feature to be enabled
    on camera, as one such permission was not enabled in CIV
    environment app icon was not showing.
    
    Solution: added auto focus permission, tested for camera
    CTS\VTS tests.
    
    Tracked-On: OAM-100470
    Signed-off-by: shivasku82 <shiva.kumara.rudrappa@intel.com>

--- groups/camera-ext/ext-camera-only/product.mk
+++ groups/camera-ext/ext-camera-only/product.mk
@@ -1,7 +1,9 @@
 # Camera: Device-specific configuration files. Supports only External USB camera, no CSI support
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:vendor/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.external.xml:vendor/etc/permissions/android.hardware.camera.external.xml \
     $(LOCAL_PATH)/{{_extra_dir}}/external_camera_config.xml:vendor/etc/external_camera_config.xml
+
 
 # External camera service
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-external-service \
--- groups/camera-ext/true/product.mk
+++ groups/camera-ext/true/product.mk
@@ -1,5 +1,6 @@
 # Camera: Device-specific configuration files.
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:vendor/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.external.xml:vendor/etc/permissions/android.hardware.camera.external.xml \
     $(LOCAL_PATH)/{{_extra_dir}}/external_camera_config.xml:vendor/etc/external_camera_config.xml
 
