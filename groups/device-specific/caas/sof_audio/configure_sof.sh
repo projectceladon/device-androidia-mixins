857a2984b8ecde2e9944cf6a21a2ad04b43aa0fd
pmandri <padmashree.mandri@intel.com>
2022-02-10 17:34:54 +0530

    Enable SOF
    Load Required snd and sof modules to enable sof in android
    and force to load sof dsp driver.
    configure_sof enables sof hda on host
    Signed-off-by: pmandri <padmashree.mandri@intel.com>

--- groups/device-specific/caas/sof_audio/configure_sof.sh
+++ groups/device-specific/caas/sof_audio/configure_sof.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-SOFBIN_TPLGS="sof-bin/lib/firmware/intel/sof-tplg-v1.4.2/"
-SOFBIN_FWS="sof-bin/lib/firmware/intel/sof/v1.4.2/"
+SOFBIN_TPLGS="sof-bin/v2.0.x/sof-tplg/"
+SOFBIN_FWS="sof-bin/v2.0.x/sof/"
 LIB_TPLG="/lib/firmware/intel/sof-tplg/"
 LIB_FW="/lib/firmware/intel/sof/"
-BLACKLIST_HDA_CONF="/etc/modprobe.d/blacklist-dsp.conf"
 SOF_WORK_DIR="$2/sof_audio"
+ALSA_CONF="/etc/modprobe.d/alsa-base.conf"
 USAGE="Usage : configure_sof.sh install/uninstall WorkingDir"
 
 function setupSof {
+     if [[ -z `grep 'options snd-intel-dspcfg' $ALSA_CONF` ]];then
+        echo "options snd-intel-dspcfg dsp_driver=3" >> $ALSA_CONF
+     else
+        sed -i 's/options snd-intel-dspcfg dsp_driver=1/options snd-intel-dspcfg dsp_driver=3/g' $ALSA_CONF
+    fi
+
     cd $SOF_WORK_DIR
     if [ -d "$LIB_FW" ]; then
         if [ ! -d "sof_bkp" ]; then
@@ -19,33 +25,27 @@ function setupSof {
             sudo mv $LIB_TPLG "sof-tplg_bkp"
         fi
     fi
-    if [ -f "$BLACKLIST_HDA_CONF" ]; then
-        if [ ! -f "blacklist_hda_bkp" ]; then
-            sudo mv $BLACKLIST_HDA_CONF "blacklist_hda_bkp"
-        fi
-    fi
     sudo mkdir $LIB_FW
     sudo mkdir $LIB_TPLG
 
     rm -rf sof-bin
-    git clone https://github.com/thesofproject/sof-bin -b stable-v1.4.2
+    git clone https://github.com/thesofproject/sof-bin -b v2.0
     if [ ! -d "sof-bin" ]; then
-        echo "Failed to download https://github.com/thesofproject/sof-bin/tree/stable-v1.4.2"
+        echo "Failed to download "
         exit -1
     fi
     sudo cp "${SOFBIN_TPLGS}sof-hda-generic-4ch.tplg" $LIB_TPLG
     sudo cp "${SOFBIN_TPLGS}sof-hda-generic-2ch.tplg" $LIB_TPLG
     sudo cp "${SOFBIN_TPLGS}sof-hda-generic.tplg" $LIB_TPLG
-    sudo cp "${SOFBIN_FWS}intel-signed/sof-cnl-v1.4.2.ri" "${LIB_FW}sof-cml.ri"
-    sudo cp "${SOFBIN_FWS}intel-signed/sof-cnl-v1.4.2.ri" "${LIB_FW}sof-cnl.ri"
-    #sudo cp "blacklist-dsp.conf" $BLACKLIST_HDA_CONF
+    sudo cp "${SOFBIN_FWS}sof-cml.ri" $LIB_FW
+    sudo cp "${SOFBIN_FWS}sof-cnl.ri" $LIB_FW
+    sudo cp "${SOFBIN_FWS}sof-adl.ri" $LIB_FW
 }
 
 function restore {
     cd $SOF_WORK_DIR
     sudo rm -rf $LIB_FW
     sudo rm -rf $LIB_TPLG
-    sudo rm $BLACKLIST_HDA_CONF
     if [ -d "sof_bkp" ]; then
         sudo mv "sof_bkp" $LIB_FW
     fi
@@ -55,6 +55,7 @@ function restore {
     if [ -f "blacklist_hda_bkp" ]; then
         sudo mv "blacklist_hda_bkp" $BLACKLIST_HDA_CONF
     fi
+    sed -i 's/options snd-intel-dspcfg dsp_driver=3/options snd-intel-dspcfg dsp_driver=1/g' $ALSA_CONF
 }
 
 if [[ ! -d $SOF_WORK_DIR ]]; then
