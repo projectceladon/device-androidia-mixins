2a06d52bb57698a36b7138b021c39a1f50c9cbf8
Swee Yee Fonn <swee.yee.fonn@intel.com>
2022-04-22 17:23:35 +0800

    WA: Force reboot type for android
    Set Android kernel commandline to force
    a,e,p,t,f reboot types.
    
    Signed-off-by: Swee Yee Fonn <swee.yee.fonn@intel.com>

--- groups/device-specific/caas/BoardConfig.mk
+++ groups/device-specific/caas/BoardConfig.mk
@@ -7,6 +7,7 @@ BOARD_KERNEL_CMDLINE += \
 	i915.hpd_sense_invert=0x7 \
 	intel_iommu=off \
 	i915.enable_pvmmio=0 \
+	reboot=a,e,p,t,f \
 	loop.max_part=7
 
 BOARD_FLASHFILES += ${TARGET_DEVICE_DIR}/bldr_utils.img:bldr_utils.img
