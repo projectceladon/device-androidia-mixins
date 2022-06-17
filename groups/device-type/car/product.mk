060e93bc6a041f98c463ea408171fffdd2c0e133
Chen Luhai <luhai.chen@intel.com>
2022-06-17 18:59:51 +0800

    remove the radio modules not support on NUC
    Tracked-On: OAM-102506
    Signed-off-by: Chen Luhai <luhai.chen@intel.com>

--- groups/device-type/car/product.mk
+++ groups/device-type/car/product.mk
@@ -3,7 +3,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.type.automotive.xml:vendor/etc/permissions/android.hardware.type.automotive.xml \
     frameworks/native/data/etc/android.hardware.screen.landscape.xml:vendor/etc/permissions/android.hardware.screen.landscape.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:vendor/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.broadcastradio.xml:vendor/etc/permissions/android.hardware.broadcastradio.xml \
     frameworks/native/data/etc/android.software.activities_on_secondary_displays.xml:vendor/etc/permissions/android.software.activities_on_secondary_displays.xml \
     $(INTEL_PATH_COMMON)/framework/android.software.cant_save_state.xml:vendor/etc/permissions/android.software.cant_save_state.xml
 
@@ -13,7 +12,6 @@ PRODUCT_PACKAGE_OVERLAYS += $(INTEL_PATH_COMMON)/device-type/overlay-car
 $(call inherit-product, packages/services/Car/car_product/build/car.mk)
 
 PRODUCT_PACKAGES += \
-    radio.fm.default \
     CarSettings \
     VmsPublisherClientSample \
     VmsSubscriberClientSample \
