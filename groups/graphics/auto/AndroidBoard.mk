3780e79d9c32416eacd40243bf3698f6da74b111
Fei Jiang <fei.jiang@intel.com>
2022-09-06 23:27:28 +0800

    remove i915 fw and unused gfx libs
    Change-Id: Id3d5989fe90a5b953660af0540b620a627a6a67f
    Signed-off-by: Fei Jiang <fei.jiang@intel.com>

--- groups/graphics/auto/AndroidBoard.mk
+++ groups/graphics/auto/AndroidBoard.mk
@@ -1,12 +1,5 @@
 {{#gen9+}}
 
-I915_FW_PATH := vendor/linux/firmware/i915
-
-#list of i915/huc_xxx.bin i915/dmc_xxx.bin i915/guc_xxx.bin
-$(foreach t, $(patsubst $(I915_FW_PATH)/%,%,$(wildcard $(I915_FW_PATH)/*)) ,$(eval I915_FW += i915/$(t)) $(eval $(LOCAL_KERNEL) : $(PRODUCT_OUT)/vendor/firmware/i915/$(t)))
-
-_EXTRA_FW_ += $(I915_FW)
-
 AUTO_IN += $(TARGET_DEVICE_DIR)/{{_extra_dir}}/auto_hal.in
 
 {{/gen9+}}
--- groups/graphics/auto/product.mk
+++ groups/graphics/auto/product.mk
@@ -1,41 +1,14 @@
 PRODUCT_PACKAGES += \
     libEGL_swiftshader \
     libGLESv1_CM_swiftshader \
-    libGLESv2_swiftshader \
-    libGLES_android \
-    libigdrcl \
-    libOpenCL \
-    libcommon_clang \
-    libigc \
-    libigdfcl
+    libGLESv2_swiftshader
 
-#Gallium drivers since mesa 22.0.3
-PRODUCT_PACKAGES += \
-    libEGL_mesa \
-    libGLESv1_CM_mesa \
-    libGLESv2_mesa \
-    libgallium_dri \
-    libglapi
-#Vulkan driver since mesa 22.0.3
-PRODUCT_PACKAGES += \
-    vulkan.intel
-
-#Keep legacy mesa driver for compatibility
-PRODUCT_PACKAGES += \
-    libGLES_mesa
 
 PRODUCT_PACKAGES += \
-    libdrm \
-    libdrm_intel \
     libsync
-#   libmd
-#Epic mdapi was deprecated for TGL and ADL platform
-#So disable libmd and leave mdapi compiling issue with mesa 22.0.3
 
 PRODUCT_PACKAGES += \
     gralloc.default
-
-PRODUCT_PACKAGES += ufo_prebuilts
 
 #Surface Flinger related Properties
 
@@ -45,26 +18,18 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.max_frame_buffer_acquir
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.vsync_event_phase_offset_ns=7500000
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.vsync_sf_event_phase_offset_ns=3000000
 
-# i915 firmwares
-$(foreach fw,$(I915_FW),$(eval PRODUCT_PACKAGES += $(notdir $(fw))))
-
 # move configure files provided by intel to vendor partition
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/{{_extra_dir}}/drirc:vendor/etc/drirc
 
 # DRM HWComposer
 PRODUCT_PACKAGES += \
-    hwcomposer.drm_minigbm \
     hwcomposer.ranchu
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.hwcomposer=ranchu
 
 {{#minigbm}}
-# Mini gbm
-PRODUCT_PACKAGES += \
-    gralloc.$(TARGET_GFX_INTEL)
-
 PRODUCT_PACKAGES += \
     TencentIME \
     tencent_virtual_input \
@@ -82,13 +47,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.gralloc=tencent
-{{/minigbm}}
-
-{{^minigbm}}
-
-#Gralloc
-PRODUCT_PACKAGES += \
-    gralloc.drm
 {{/minigbm}}
 
 {{^gen9+}}
@@ -131,7 +89,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:vendor/etc/permissions/android.software.vulkan.deqp.level.xml
 
 PRODUCT_PACKAGES += \
-    vulkan.$(TARGET_BOARD_PLATFORM) \
     vulkan.pastel \
     vulkan.ranchu
 {{/vulkan}}
