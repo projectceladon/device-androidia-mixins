59ef18a9b3c12a3fcf7a5c3900ec7ca8b1235c80
Rajani Ranjan <rajani.ranjan@intel.com>
2023-01-16 11:09:37 +0530

    mixin changes to support health aidl hal .
    health package is customized from
    android.hardware.health-service.example
    to android.hardware.health-service.intel
    
    Tracked-On: OAM-105769
    Signed-off-by: Rajani Ranjan <rajani.ranjan@intel.com>

--- groups/health/hal/product.mk
+++ groups/health/hal/product.mk
@@ -1,2 +1,2 @@
-PRODUCT_PACKAGES +=   android.hardware.health-service.example \
-                      android.hardware.health@2.1-impl-intel
+PRODUCT_PACKAGES +=   android.hardware.health-service.intel
+
