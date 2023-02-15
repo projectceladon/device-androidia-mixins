071c8f709a1aa68d05e7df858af20afc7855349c
Jaikrishna, Nemallapudi <nemallapudi.jaikrishna@intel.com>
2023-02-15 17:42:57 +0530

    Add mixin support for neuralnetworks HAL
    Tracked-On: OAM-105852
    Signed-off-by: Jaikrishna, Nemallapudi <nemallapudi.jaikrishna@intel.com>
    Signed-off-by: Ratnesh Kumar Rai <ratnesh.kumar.rai@intel.com>

---
+++ groups/neuralnetworks/true/product.mk
@@ -0,0 +1,15 @@
+# neuralnetworks HAL
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3-generic-service \
+    android.hardware.neuralnetworks@1.3-generic-impl \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/{{_extra_dir}}/plugins.xml:system/vendor/etc/openvino/plugins.xml \
+
+
+PRODUCT_PACKAGES += \
+    libopenvino \
+    libopenvino_intel_cpu_plugin \
+    libopenvino_ir_frontend
+
+PRODUCT_PROPERTY_OVERRIDES += vendor.nn.hal.ngraph=true
