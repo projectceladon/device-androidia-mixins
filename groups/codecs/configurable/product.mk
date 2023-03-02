4b6f512625cc61ccc09e5b27885e957261d9ee74
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2023-03-02 09:37:21 +0530

    Add support for codec2.0
    Changes include
    - Added packages and configuration files for Codec2.0
    - Added option flags to enable/disable media and its
    dependent packages.
    
    Tracked-On: OAM-108761
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- groups/codecs/configurable/product.mk
+++ groups/codecs/configurable/product.mk
@@ -1,7 +1,15 @@
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/{{_extra_dir}}/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/{{_extra_dir}}/mfx_omxil_core.conf:system/vendor/etc/mfx_omxil_core.conf \
-    $(LOCAL_PATH)/{{_extra_dir}}/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/{{_extra_dir}}/media_profiles.xml:system/vendor/etc/media_profiles_V1_0.xml \
-    $(LOCAL_PATH)/{{_extra_dir}}/media_codecs.xml:system/vendor/etc/media_codecs.xml
+    $(LOCAL_PATH)/{{_extra_dir}}/media_profiles.xml:system/etc/media_profiles.xml \
 
+{{#hw_omx_video}}
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/{{_extra_dir}}/media_codecs_performance.xml:system/vendor/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/{{_extra_dir}}/media_codecs.xml:system/vendor/etc/media_codecs.xml \
+    $(LOCAL_PATH)/{{_extra_dir}}/mfx_omxil_core.conf:system/vendor/etc/mfx_omxil_core.conf
+{{/hw_omx_video}}
+
+{{#sw_omx_video}}
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/vendor/etc/media_codecs_google_video.xml
+{{/sw_omx_video}}
