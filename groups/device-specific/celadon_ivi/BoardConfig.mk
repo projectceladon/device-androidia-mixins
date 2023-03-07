ff9dc5d67a5354c252cf95dad158809c237360b1
Qi Zhang <qi1.zhang@intel.com>
2023-03-07 17:50:07 +0800

    remove unknown kernel command line
    Tracked-On: OAM-108844
    Signed-off-by: Qi Zhang <qi1.zhang@intel.com>

--- groups/device-specific/celadon_ivi/BoardConfig.mk
+++ groups/device-specific/celadon_ivi/BoardConfig.mk
@@ -3,7 +3,6 @@ DEVICE_PACKAGE_OVERLAYS += ${TARGET_DEVICE_DIR}/overlay
 BOARD_KERNEL_CMDLINE += \
 	no_timer_check \
 	noxsaves \
-	reboot_panic=p,w \
 	i915.hpd_sense_invert=0x7 \
 	intel_iommu=off \
 	i915.enable_pvmmio=0 \
