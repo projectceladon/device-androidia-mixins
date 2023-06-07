4235e23998cec6e87573bf656b92c5c0da989694
Austin Sun <austin.sun@intel.com>
2023-06-07 08:35:55 +0800

    feat: add silent mode in mix option and
    enable kernel config CONFIG_PM_SILENTMODE
    
    Tracked-On: OAM-112399
    Signed-off-by: Austin Sun <austin.sun@intel.com>

--- groups/kernel/BoardConfig.mk
+++ groups/kernel/BoardConfig.mk
@@ -38,6 +38,13 @@ BOARD_KERNEL_CMDLINE += \
 BOARD_KERNEL_CMDLINE += \
         pm_suspend_debug=1
 {{/pmsuspend_debug}}
+
+{{#pmsilent_mode}}
+BOARD_KERNEL_CMDLINE += \
+        silentmode={{pmsilent_mode}}
+{{/pmsilent_mode}}
+
+
 {{#memory_hole}}
 BOARD_KERNEL_CMDLINE += \
         memmap=4M\$$0x5c400000
