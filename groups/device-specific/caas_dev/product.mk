16439edd53131f263844b9f845aebfacbe439fd9
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2023-06-12 21:02:55 +0530

    Manifest and package changes for minigbm AIDL HAL
    Changes include:
    - Removal of allocator and mapper entries in manifest.xml as those
    are added in minigbm project.
    - Added libminigbm_gralloc_intel package
    - Updated allocator service and mapper library names
    
    Tracked-On: OAM-112813
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
    Signed-off-by: HeYue <yue.he@intel.com>

--- groups/device-specific/caas_dev/product.mk
+++ groups/device-specific/caas_dev/product.mk
@@ -26,8 +26,8 @@ PRODUCT_PACKAGES +=  \
                     android.hardware.usb-service.example \
                     camera.device@1.0-impl \
                     android.hardware.camera.provider@2.4-impl \
-                    android.hardware.graphics.mapper@4.0-impl.minigbm \
-                    android.hardware.graphics.allocator@4.0-service.minigbm \
+                    android.hardware.graphics.mapper@4.0-impl.minigbm_intel \
+                    android.hardware.graphics.allocator@4.0-service.minigbm_intel \
                     android.hardware.renderscript@1.0-impl \
                     android.hardware.graphics.composer@2.4-service \
                     android.hardware.identity \
