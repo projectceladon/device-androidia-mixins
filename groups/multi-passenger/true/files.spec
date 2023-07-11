5de98cf8ab2d2d046b282ad6dd9151bdb4173ff5
Deepa K G <g.k.deepa@intel.com>
2023-07-11 19:06:13 +0530

    Manage audio configs in multizone enable/disable
    DynamicRouting should be true in both the cases as we
    are supporting BUS based audio HAL
    
    Tracked-On: OAM-111146
    Signed-off-by: Deepa K G <g.k.deepa@intel.com>

--- groups/multi-passenger/true/files.spec
+++ groups/multi-passenger/true/files.spec
@@ -1,3 +1,4 @@
 [devicefiles]
 overlay-multi_passenger : "multi-passenger overlay configuration"
+audio : "audio related configuration files"
 
--- groups/multi-passenger/true/product.mk
+++ groups/multi-passenger/true/product.mk
@@ -1,2 +1,6 @@
 # multi-passenger support
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-multi_passenger
+# Vendor audio configuration files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/overlay-multi_passenger/audio/audio_policy_configuration.xml:vendor/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/overlay-multi_passenger/audio/car_audio_configuration.xml:vendor/etc/car_audio_configuration.xml
