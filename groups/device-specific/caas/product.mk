82a9590fc991d75914a00369280802ac34abf58f
Tanuj Tekriwal <tanuj.tekriwal@intel.com>
2023-08-31 08:53:21 +0000

    Enable omx service although hal is deprecated
    OMX service is needed even if hal is deprecated
    Since decoder depends on omx apis.
    
    Tracked-On: OAM-112815
    Signed-off-by: Tanuj Tekriwal <tanuj.tekriwal@intel.com>

--- groups/device-specific/caas/product.mk
+++ groups/device-specific/caas/product.mk
@@ -32,6 +32,7 @@ PRODUCT_PACKAGES +=  \
                     android.hardware.identity \
                     android.hardware.identity-service.example \
                     android.hardware.graphics.composer3-service.intel \
+		    android.hardware.media.omx@1.0-service
 
 
 PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
