6598d5812ed687e63f258b5cbf9b9416c1bc1997
shivasku82 <shiva.kumara.rudrappa@intel.com>
2023-09-19 14:09:21 +0530

    camera-ext/ivi: Add camera service and config file
    Added the required packages, configuration file for camera HAL
    used in IVI.
    
    Tracked-On: OAM-112309
    Signed-off-by: shivasku82 <shiva.kumara.rudrappa@intel.com>

--- groups/camera-ext/ivi/product.mk
+++ groups/camera-ext/ivi/product.mk
@@ -2,12 +2,13 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.autofocus.xml:vendor/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.external.xml:vendor/etc/permissions/android.hardware.camera.external.xml \
-    $(LOCAL_PATH)/{{_extra_dir}}/external_camera_config.xml:vendor/etc/external_camera_config.xml
+    $(LOCAL_PATH)/{{_extra_dir}}/external_camera_config.xml:vendor/etc/external_camera_config.xml \
+    $(LOCAL_PATH)/{{_extra_dir}}/ivi_camera_config.xml:vendor/etc/ivi_camera_config.xml
 
 
 # External camera service
-PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-external-service \
-                    android.hardware.camera.provider@2.4-impl
-
+PRODUCT_PACKAGES += android.vendor.hardware.camera.provider@2.4-ivi-service \
+		     android.vendor.hardware.camera.provider@2.4-external-service \
+                    android.hardware.camera.provider@2.4-impl-intel
 
 PRODUCT_PACKAGES += MultiCameraApp
