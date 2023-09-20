91ea426e3e06996ee9e1397fc9ce8d5029490e82
Lin, Ziniu <ziniu.lin@intel.com>
2023-09-20 06:22:22 +0000

    Add docker mixins group
    This docker mixins group added docker runtime binaries and
    configurations files which provide running docker containers
    features for Android.
    
    Tracked-On: OAM-112312
    Signed-off-by: Lin, Ziniu <ziniu.lin@intel.com>
    Signed-off-by: Wang, Liang <liang.wang@intel.com>

---
+++ groups/docker/true/AndroidBoard.mk
@@ -0,0 +1,15 @@
+DOCKERD_ENV_RESOLV_CONF := $(PRODUCT_OUT)/system/etc/resolv.conf
+$(DOCKERD_ENV_RESOLV_CONF):
+	@ln -sf /data/vendor/docker/etc/resolv.conf $(PRODUCT_OUT)/system/etc/resolv.conf
+$(PRODUCT_OUT)/system.img:  $(DOCKERD_ENV_RESOLV_CONF)
+
+LIC_IMAGE_SOURCE := $(PRODUCT_OUT)/vendor/etc/docker/gamecore.tar
+$(LIC_IMAGE_SOURCE):
+	@mkdir -p $(PRODUCT_OUT)/vendor/etc/docker
+ifneq ($(wildcard $(TOP)/vendor/intel/weston-in-docker/gamecore),)
+	@tar --exclude .git -cf $(PRODUCT_OUT)/vendor/etc/docker/gamecore.tar -C $(TOP)/vendor/intel/weston-in-docker/gamecore .
+endif
+ifneq ($(wildcard $(TOP)/vendor/intel/weston-in-docker/aicore),)
+	@tar --exclude .git -cf $(PRODUCT_OUT)/vendor/etc/docker/aicore.tar -C $(TOP)/vendor/intel/weston-in-docker/aicore .
+endif
+$(PRODUCT_OUT)/vendor.img: $(LIC_IMAGE_SOURCE)
---
+++ groups/docker/true/product.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/{{_extra_dir}}/lic_install.sh:/vendor/bin/lic_install.sh \
+    $(LOCAL_PATH)/{{_extra_dir}}/docker_manager:/vendor/bin/docker_manager \
+    $(LOCAL_PATH)/{{_extra_dir}}/containerd:/vendor/bin/containerd \
+    $(LOCAL_PATH)/{{_extra_dir}}/containerd-shim-runc-v2:/vendor/bin/containerd-shim-runc-v2 \
+    $(LOCAL_PATH)/{{_extra_dir}}/docker:/vendor/bin/docker \
+    $(LOCAL_PATH)/{{_extra_dir}}/dockerd:/vendor/bin/dockerd \
+    $(LOCAL_PATH)/{{_extra_dir}}/dockerd-dev:/vendor/bin/dockerd-dev \
+    $(LOCAL_PATH)/{{_extra_dir}}/runc:/vendor/bin/runc \
+    $(LOCAL_PATH)/{{_extra_dir}}/99-ignore-keyboard.rules:/vendor/etc/docker/config/99-ignore-keyboard.rules \
+    $(LOCAL_PATH)/{{_extra_dir}}/99-ignore-mouse.rules:/vendor/etc/docker/config/99-ignore-mouse.rules \
+    $(LOCAL_PATH)/{{_extra_dir}}/config.toml:/vendor/etc/docker/etc/containerd/config.toml \
+    $(LOCAL_PATH)/{{_extra_dir}}/daemon.json:/vendor/etc/docker/etc/docker/daemon.json
