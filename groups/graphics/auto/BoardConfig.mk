da321612f680444a788e035dbcceebe8fbb16ba5
chenyanxzhu <chenyanx.zhu@intel.com>
2023-09-25 11:10:01 +0800

    disable fbc

--- groups/graphics/auto/BoardConfig.mk
+++ groups/graphics/auto/BoardConfig.mk
@@ -2,7 +2,7 @@
 TARGET_USE_PRIVATE_LIBDRM := true
 LIBDRM_VER ?= intel
 
-BOARD_KERNEL_CMDLINE += i915.modeset=1 drm.atomic=1 i915.nuclear_pageflip=1 drm.vblankoffdelay=1 i915.fastboot=1
+BOARD_KERNEL_CMDLINE += i915.modeset=1 drm.atomic=1 i915.nuclear_pageflip=1 drm.vblankoffdelay=1 i915.fastboot=1 i915.enable_fbc=0
 {{^acrn-guest}}
 {{#enable_guc}}
 BOARD_KERNEL_CMDLINE += i915.enable_guc=2
