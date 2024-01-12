7d11c9433ff464e0166c44fa5255a1ec61fa0f71
Zhong,Fangjian <fangjian.zhong@intel.com>
2024-01-12 19:17:21 +0800

    Add logcat2hvc build dependency
    logcat2hvc depends on logsvc selinux policy, and logsvc
    is defined in crashlogd. Add dependency to ensure logcat2hvc
    would be built only with non user-build and debug-crashlogd
    mixin group enabled.
    
    Test Done: Boot check
    
    Signed-off-by: Zhong,Fangjian <fangjian.zhong@intel.com>
    Tracked-On: OAM-114762

--- groups/debug-tools/true/BoardConfig.mk
+++ groups/debug-tools/true/BoardConfig.mk
@@ -1,7 +1,9 @@
 BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/debug-tools/androidterm
 
+ifeq ($(MIXIN_DEBUG_LOGS),true)
 {{#logcat2hvc}}
 BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/logcat2hvc
 
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 {{/logcat2hvc}}
+endif
