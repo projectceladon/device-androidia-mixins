21c392a0a5d56ca19ddd1265591f2141f450c56f
Deng, Bing <bing.deng@intel.com>
2024-04-16 14:07:57 +0800

    Remove android.hardware.sensor.ambient_temperature.
    android.hardware.sensor.ambient_temperature is removed.
    
    Tracked-On: OAM-117336
    Signed-off-by: Deng, Bing <bing.deng@intel.com>

--- groups/sensors/mediation/product.mk
+++ groups/sensors/mediation/product.mk
@@ -8,7 +8,6 @@ PRODUCT_PACKAGES += \
 
 {{#enable_sensor_list}}
 PRODUCT_COPY_FILES += \
-        frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:vendor/etc/permissions/android.hardware.sensor.ambient_temperature.xml \
         frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:vendor/etc/permissions/android.hardware.sensor.accelerometer.xml \
         frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:vendor/etc/permissions/android.hardware.sensor.gyroscope.xml \
         frameworks/native/data/etc/android.hardware.sensor.compass.xml:vendor/etc/permissions/android.hardware.sensor.compass.xml \
