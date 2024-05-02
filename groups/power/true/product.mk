00f60608123fc9cf72f9c266c87265c65621321d
Shwetha B <shwetha.b@intel.com>
2024-05-02 14:21:17 +0530

    power optimization for idle display
    Push the android power tune script to vendor/bin
    to achieve the following -
    Opportunistic low power configuration
    Link Power Management Policy
    NMI watchdog disable
    Power Save setting for HD-Audio driver
    
    Tracked-On: OAM-123276
    Signed-off-by: Shwetha B <shwetha.b@intel.com>

--- groups/power/true/product.mk
+++ groups/power/true/product.mk
@@ -2,4 +2,4 @@
 PRODUCT_PACKAGES += power.$(TARGET_BOARD_PLATFORM) \
                     android.hardware.power-service.example \
                     power_hal_helper
-
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/extra_files/power/android_pm_tune.sh:vendor/bin/android_pm_tune.sh
