bd52a4681ba01a3c1a8cd4f1692030c89f533a79
sgnanase <sundar.gnanasekaran@intel.com>
2024-05-27 11:34:58 +0530

    Ensure we compile audio_aaos HAL for base_aaos
    Tracked-On: OAM-118842
    Signed-off-by: Deepa K G <g.k.deepa@intel.com>
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- groups/audio/audio_base_aaos/BoardConfig.mk
+++ groups/audio/audio_base_aaos/BoardConfig.mk
@@ -6,7 +6,7 @@ ifneq ($(BOARD_USES_GENERIC_AUDIO), true)
 # Audio HAL selection Flag default setting.
 #  INTEL_AUDIO_HAL:= audio     -> baseline HAL
 #  INTEL_AUDIO_HAL:= audio_pfw -> PFW-based HAL
-INTEL_AUDIO_HAL := audio
+INTEL_AUDIO_HAL := aaos
 else
 INTEL_AUDIO_HAL := stub
 endif
