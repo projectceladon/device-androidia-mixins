8dae1575401db0c2d011d09abb2edfb044ac5801
RajaniRanjan <rajani.ranjan@intel.com>
2024-06-26 10:13:53 +0530

    adding required uncalibrated sensors xml files for automotive
    enabling limited_axis uncalibrated features.
    
    Tracked-On: OAM-118805
    Signed-off-by: RajaniRanjan <rajani.ranjan@intel.com>

--- groups/sensors/mediation/product.mk
+++ groups/sensors/mediation/product.mk
@@ -14,7 +14,9 @@ PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.sensor.compass.xml:vendor/etc/permissions/android.hardware.sensor.compass.xml \
         frameworks/native/data/etc/android.hardware.sensor.light.xml:vendor/etc/permissions/android.hardware.sensor.light.xml \
 	frameworks/native/data/etc/android.hardware.sensor.gyroscope_limited_axes.xml:vendor/etc/permissions/android.hardware.sensor.gyroscope_limited_axes.xml \
-	frameworks/native/data/etc/android.hardware.sensor.accelerometer_limited_axes.xml:vendor/etc/permissions/android.hardware.sensor.accelerometer_limited_axes.xml
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer_limited_axes.xml:vendor/etc/permissions/android.hardware.sensor.accelerometer_limited_axes.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer_limited_axes_uncalibrated.xml:vendor/etc/permissions/android.hardware.sensor.accelerometer_limited_axes_uncalibrated.xml \
+	frameworks/native/data/etc/android.hardware.sensor.gyroscope_limited_axes_uncalibrated.xml:vendor/etc/permissions/android.hardware.sensor.gyroscope_limited_axes_uncalibrated.xml
 
 AUTO_IN += $(TARGET_DEVICE_DIR)/{{_extra_dir}}/auto_hal.in
 {{/enable_sensor_list}}
