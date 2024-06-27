e6a7e69090b49471ab30989a5a884f1375fe3491
Neo Fang <neo.fang@intel.com>
2024-06-27 04:09:24 +0000

    Set camera back facing
    Issue Detailed: Android14 CDD defines that it's mandatory to have
    a primary rear facing camera with a solution of at least 12M pixels
    supporting.
    
    Fix Detailed: set the camera as back facing.
    
    Tested-On: CTS/Preview/Capture Intel POC platform.
    
    Tracked-On: OAM-121415
    Signed-off-by: Neo Fang <neo.fang@intel.com>

--- groups/camera-ext/base_aaos/product.mk
+++ groups/camera-ext/base_aaos/product.mk
@@ -1,8 +1,7 @@
 # Camera: Device-specific configuration files.
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.autofocus.xml:vendor/etc/permissions/android.hardware.camera.autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.external.xml:vendor/etc/permissions/android.hardware.camera.external.xml \
-    $(LOCAL_PATH)/{{_extra_dir}}/external_camera_config.xml:vendor/etc/external_camera_config.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:vendor/etc/permissions/android.hardware.camera.xml \
     $(LOCAL_PATH)/{{_extra_dir}}/ivi_camera_config.xml:vendor/etc/ivi_camera_config.xml
 
 # External camera service
