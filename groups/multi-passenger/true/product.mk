d16b0b33a0ff857c23213202e73b0ccdc0fec8cf
Deepa K G <g.k.deepa@intel.com>
2024-07-02 13:29:54 +0000

    Multizone audio enabling
    The change are to include multiple occupant zone to support
    1. main user -> audio zone 0 -> occupant 0
    2. front passenger -> audio zone 1 -> occupant 1
    
    Tests:
    Tested with Kitchen sink app
    Tested by playing audio/video file from two different users
    simultaneous playback from two zones was tested
    
    Tracked-On: OAM-121930
    Signed-off-by: Deepa K G <g.k.deepa@intel.com>

--- groups/multi-passenger/true/product.mk
+++ groups/multi-passenger/true/product.mk
@@ -7,7 +7,16 @@ PRODUCT_PACKAGES += CarServiceMultiDisplayOverlay \
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-multi_passenger
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.managed_users.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.managed_users.xml
+    frameworks/native/data/etc/android.software.managed_users.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.managed_users.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:vendor/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/car_audio_configuration.xml:vendor/etc/car_audio_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration_attached_devices.xml:vendor/etc/audio_policy_configuration_attached_devices.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration_devices.xml:vendor/etc/audio_policy_configuration_devices.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration_mixports.xml:vendor/etc/audio_policy_configuration_mixports.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration_routes.xml:vendor/etc/audio_policy_configuration_routes.xml \
+    $(LOCAL_PATH)/audio/audio_hal_configuration.xml:vendor/etc/audio_hal_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_hal_configuration_intel_poc.xml:vendor/etc/audio_hal_configuration_intel_poc.xml 
+
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/display_settings.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display_settings.xml
