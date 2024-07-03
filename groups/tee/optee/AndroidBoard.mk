0481872398ee4c86191fb0ff1786507b170f1c7a
jizhenlo <zhenlong.z.ji@intel.com>
2024-07-03 14:25:04 +0800

    Change teedata partition mount point to /mnt/vendor/persist
    Currently teedata partition's mount point is /persist. Vendor
    dir under rootfs may be overridden after flashing the GSI
    image, and teedata partition cannot be mounted any longer.
    
    Test done: tee clients work normally after flashing GSI image.
    
    Tracked-On: OAM-121938
    Signed-off-by: jizhenlo <zhenlong.z.ji@intel.com>

--- groups/tee/optee/AndroidBoard.mk
+++ groups/tee/optee/AndroidBoard.mk
@@ -1,8 +1,2 @@
-# /persist is using for the optee secure storage
-make_persist_dir:
-	@mkdir -p $(PRODUCT_OUT)/root/persist
-
-$(PRODUCT_OUT)/ramdisk.img: make_persist_dir
-
 # OPTEE_ELF triggered by keymaster TA in optee-os
 $(OPTEE_ELF) : dba51a17-0563-11e7-93b1-6fa7b0071a51.ta
