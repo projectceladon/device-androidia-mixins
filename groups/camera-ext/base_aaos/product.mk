297c228ca07e56640543c769651d782d53781abe
Xu Bing <bing.xu@intel.com>
2024-07-10 13:55:03 +0800

    Remove autofocus feature for camera
    Currently, camera use "V4L2_CID_FOCUS_ABSOLUTE" for focus distance,
    so don't support auto focus now, remove the feature.
    
    Tracked-On: OAM-119971
    Signed-off-by: Xu Bing <bing.xu@intel.com>

--- groups/camera-ext/base_aaos/product.mk
+++ groups/camera-ext/base_aaos/product.mk
@@ -1,6 +1,5 @@
 # Camera: Device-specific configuration files.
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:vendor/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.xml:vendor/etc/permissions/android.hardware.camera.xml \
     $(LOCAL_PATH)/{{_extra_dir}}/ivi_camera_config.xml:vendor/etc/ivi_camera_config.xml
 
