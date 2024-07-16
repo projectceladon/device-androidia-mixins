5d0d7197516f3eed6b35f0a9a839ec9196ff8b06
HeYue <yue.he@intel.com>
2024-07-16 16:33:21 +0800

    Add sepolicy for application rendering setting property
    Add app_render_setting_property folder to sepolicy dir.
    
    Tracked-On: OAM-122333
    Signed-off-by: HeYue <yue.he@intel.com>

--- groups/graphics/auto/BoardConfig.mk
+++ groups/graphics/auto/BoardConfig.mk
@@ -60,4 +60,7 @@ BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/graphics/mesa_acrn
 BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/graphics/opencl
 {{/opencl_sepolicy}}
 
+#Add app_render_setting_property folder to BOARD_SEPOLICY_DIRS
+BOARD_SEPOLICY_DIRS += $(INTEL_PATH_SEPOLICY)/app_render_setting_property
+
 BOARD_SEPOLICY_M4DEFS += module_hwc_info_service=true
