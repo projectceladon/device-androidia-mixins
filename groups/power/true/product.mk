3b4c1cc47755f3cc8eb8829473dc02456209496d
Shwetha B <shwetha.b@intel.com>
2024-08-27 22:07:35 +0530

    Mixin changes for Power and stats hal
    apex bundled with power and stats hal
    is being added to product packages.
    
    Adding mixin group for power-stats hal
    This can be used with the non-apex version
    if needed.
    
    Test Done: Boot and check if power stats
    and power hal are loaded
    
    Tracked-On: OAM-126029
    Signed-off-by: Shwetha B <shwetha.b@intel.com>

--- groups/power/true/product.mk
+++ groups/power/true/product.mk
@@ -1,5 +1,5 @@
 # Power HAL
 PRODUCT_PACKAGES += power.$(TARGET_BOARD_PLATFORM) \
-                    android.hardware.power-service.example \
+                    com.android.hardware.power.intel \
                     power_hal_helper
 
