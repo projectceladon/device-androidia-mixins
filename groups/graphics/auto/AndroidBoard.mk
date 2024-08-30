5e946260aba866e359bebec62ca3a57a1eb9908a
Wang, Yiyang <yiyang.wang@intel.com>
2024-08-30 01:34:59 +0000

    dkms: optimize the dg2 kernel driver loading time
    Tracked-On: OAM-124024
    Signed-off-by: Wang, Yiyang <yiyang.wang@intel.com>

--- groups/graphics/auto/AndroidBoard.mk
+++ groups/graphics/auto/AndroidBoard.mk
@@ -12,6 +12,10 @@ $(foreach t, $(patsubst $(I915_FW_PATH)/%,%,$(wildcard $(I915_FW_PATH)/adl*)) ,$
 $(foreach t, $(patsubst $(I915_FW_PATH)/%,%,$(wildcard $(I915_FW_PATH)/dg2*)) ,$(eval I915_FW += i915/$(t)) $(eval $(LOCAL_KERNEL) : $(PRODUCT_OUT)/vendor/firmware/i915/$(t)))
 $(foreach t, $(patsubst $(I915_FW_PATH)/%,%,$(wildcard $(I915_FW_PATH)/mtl*)) ,$(eval I915_FW += i915/$(t)) $(eval $(LOCAL_KERNEL) : $(PRODUCT_OUT)/vendor/firmware/i915/$(t)))
 
+I915_FW += i915/compat.ko
+I915_FW += i915/intel_vsec.ko
+I915_FW += i915/i915_ag.ko
+
 _EXTRA_FW_ += $(I915_FW)
 
 AUTO_IN += $(TARGET_DEVICE_DIR)/{{_extra_dir}}/auto_hal.in
