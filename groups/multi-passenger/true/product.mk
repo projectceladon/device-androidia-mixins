c70c6aac3e3761ac6d44a717e55b7667d586ac46
Gao, Fengqian <fengqian.gao@intel.com>
2024-09-05 03:18:56 +0000

    Disable android.software.managed_user in Android14
    android.software.managed_user is not required anymore to support multi-passenger in A14.
    
    Tracked-On: OAM-124336
    Signed-off-by: Gao, Fengqian <fengqian.gao@intel.com>

--- groups/multi-passenger/true/product.mk
+++ groups/multi-passenger/true/product.mk
@@ -12,7 +12,6 @@ PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-multi_passenger
 
 ifeq ($(AUDIO_POLICY_WITH_USB_AUDIO), true)
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.managed_users.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.managed_users.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration_usb.xml:vendor/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/car_audio_configuration.xml:vendor/etc/car_audio_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration_attached_devices.xml:vendor/etc/audio_policy_configuration_attached_devices.xml \
@@ -23,7 +22,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_hal_configuration_intel_poc.xml:vendor/etc/audio_hal_configuration_intel_poc.xml
 else
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.managed_users.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.managed_users.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:vendor/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/car_audio_configuration.xml:vendor/etc/car_audio_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration_attached_devices.xml:vendor/etc/audio_policy_configuration_attached_devices.xml \
