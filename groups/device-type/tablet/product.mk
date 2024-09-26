a7e6c0dd4a5e330c06aa49da862ca64dec7e5c02
Tanuj Tekriwal <tanuj.tekriwal@intel.com>
2024-09-26 08:10:16 +0000

    Revert "Enable Android Desktop mode by default"
    This reverts commit 0e89b8e41107c3f9c5a795a3a183d9a2c29fcec9.
    
    Tracked-On: OAM-125619
    Signed-off-by: Tanuj Tekriwal <tanuj.tekriwal@intel.com>

--- groups/device-type/tablet/product.mk
+++ groups/device-type/tablet/product.mk
@@ -1,8 +1,5 @@
-PRODUCT_CHARACTERISTICS := pc
+PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_COPY_FILES += \
-        {{{pc_core_hardware_path}}}/pc_core_hardware.xml:vendor/etc/permissions/pc_core_hardware.xml
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:vendor/etc/permissions/android.software.freeform_window_management.xml
+        {{{tablet_core_hardware_path}}}/tablet_core_hardware.xml:vendor/etc/permissions/tablet_core_hardware.xml
 
