b0d8f69e3768fd49a878c186d1fd41f9da27cd34
Martin, Chen <haochuan.z.chen@intel.com>
2024-10-09 07:51:33 +0800

    bsp_diff cleanup to mixin
    Tests done:
    1. Media playback and capture
    2. Video playback
    3. HFP call
    
    Tracked-On: OAM-125159
    Signed-off-by: Haochuan Z Chen <haochuan.z.chen@intel.com>

--- groups/multi-passenger/true/product.mk
+++ groups/multi-passenger/true/product.mk
@@ -31,7 +31,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration_mixports.xml:vendor/etc/audio_policy_configuration_mixports.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration_routes.xml:vendor/etc/audio_policy_configuration_routes.xml \
     $(LOCAL_PATH)/audio/audio_hal_configuration.xml:vendor/etc/audio_hal_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_hal_configuration_intel_poc.xml:vendor/etc/audio_hal_configuration_intel_poc.xml 
+    $(LOCAL_PATH)/audio/audio_hal_configuration_intel_poc.xml:vendor/etc/audio_hal_configuration_intel_poc.xml
 endif
 
 PRODUCT_COPY_FILES += \
