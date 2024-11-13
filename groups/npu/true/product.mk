61de714fdd0d475623b0a7451bc76102e7e5d0d9
JeevakaPrabu <jeevaka.badrappan@intel.com>
2024-11-13 12:11:37 +0000

    Add openvino, npu driver and compiler packages
    Tests done:
    - Android boot
    - Used hello_query_device and benchmark_app to validate device
    detection and inference.
    
    Tracked-On: OAM-127337
    Signed-off-by: JeevakaPrabu <jeevaka.badrappan@intel.com>

--- groups/npu/true/product.mk
+++ groups/npu/true/product.mk
@@ -3,3 +3,22 @@ LOCAL_NPU_FIRMWARE_DIR := hardware/intel/external/linux-npu-driver/firmware/bin
 PRODUCT_COPY_FILES += \
     $(LOCAL_NPU_FIRMWARE_DIR)/vpu_37xx_v0.0.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/intel/vpu/vpu_37xx_v0.0.bin \
     $(LOCAL_NPU_FIRMWARE_DIR)/vpu_40xx_v0.0.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/intel/vpu/vpu_40xx_v0.0.bin
+
+PRODUCT_PACKAGES += \
+    libnpu_driver_compiler \
+    libze_loader \
+    libze_intel_vpu \
+
+PRODUCT_PACKAGES += \
+    libc++_shared \
+    libopenvino \
+    libopenvino_intel_gpu_plugin \
+    libopenvino_intel_npu_plugin \
+    libopenvino_ir_frontend \
+    libopenvino_tensorflow_lite_frontend
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PACKAGES += \
+    benchmark_app \
+    hello_query_device
+endif
