cc7f550903217bc9040cc2675d0baf456aca4da7
Chen, Gang G <gang.g.chen@intel.com>
2024-12-03 14:35:12 +0800

    Support dynamically loadable kernel module for system/vendor/odm
    GAS Vendor Software Requirements 4.0:
    Automotive device implementations launched with Android 13
    or higher and running kernel version 5.15.41 or higher
    [G-0-777] MUST have the vendor_dlkm dynamic partition
    and the vendor_boot static partition.
    The patch enables DLKM for system/vendor/odm partitions
    
    Test Done:
    Boot
    vts_dlkm_partition_test
    
    Ori-Tracked-On: OAM-118469
    Tracked-On: OAM-128175
    Signed-off-by: Chen, Gang G <gang.g.chen@intel.com>

--- groups/odm-partition/true/BoardConfig.mk
+++ groups/odm-partition/true/BoardConfig.mk
@@ -3,10 +3,17 @@
 TARGET_COPY_OUT_ODM := odm
 BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := {{system_fs}}
 ODM_PARTITION_SIZE := $(shell echo {{partition_size}}*1048576 | bc)
+
+# odm_dlkm support
+BOARD_USES_ODM_DLKMIMAGE := true
+BOARD_ODM_DLKMIMAGE_FILE_SYSTEM_TYPE := {{system_fs}}
+TARGET_COPY_OUT_ODM_DLKM := odm_dlkm
+
 {{^dynamic-partitions}}
 BOARD_ODMIMAGE_PARTITION_SIZE := $(ODM_PARTITION_SIZE)
 {{/dynamic-partitions}}
 TARGET_USE_ODM := true
 {{#slot-ab}}
 AB_OTA_PARTITIONS += odm
+AB_OTA_PARTITIONS += odm_dlkm
 {{/slot-ab}}
