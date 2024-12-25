d222e90e6ebd54a51c18b247460aa5fae63adb05
Qi Zhang <qi1.zhang@intel.com>
2024-12-25 13:09:54 +0800

    disable thermal-daemon in AAOS VM
    thermal-daemon shall be enabled in SOS. Keep thermal hal
    only for AAOS.
    
    Tracked-On: OAM-128859
    Signed-off-by: Qi Zhang <qi1.zhang@intel.com>

--- groups/thermal/hal/product.mk
+++ groups/thermal/hal/product.mk
@@ -1,4 +1,2 @@
 # Thermal Hal
-PRODUCT_PACKAGES += thermal.$(TARGET_BOARD_PLATFORM) \
-                    android.hardware.thermal@2.0-service.intel \
-                    android.hardware.thermal@2.0-impl.intel
+PRODUCT_PACKAGES += android.hardware.thermal@aidl-service.intel
