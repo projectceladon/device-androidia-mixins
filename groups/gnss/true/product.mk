053e32ccf435c4c6a7b5ea1f52ab02ca13816a13
Zhenlong Z Ji <zhenlong.z.ji@intel.com>
2024-12-27 14:30:51 +0800

    Add location gps feature when gnss is enabled
    Tracked-On: OAM-128934
    Signed-off-by: Zhenlong Z Ji <zhenlong.z.ji@intel.com>

--- groups/gnss/true/product.mk
+++ groups/gnss/true/product.mk
@@ -1,6 +1,8 @@
 PRODUCT_PACKAGES += \
 	android.hardware.gnss-service.intel
 
+PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.location.gps.xml:vendor/etc/permissions/android.hardware.location.gps.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	vendor.ser.gnss-uart=/dev/ttyUSB0
