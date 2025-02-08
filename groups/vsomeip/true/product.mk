90ddc9562d1d47cf4be3e0ef8a1371ff8e9feb62
Qi Zhang <qi1.zhang@intel.com>
2025-02-08 09:53:57 +0800

    add vsomeip sample for test
    Test Done:
    Start service and client via different adb shell sessions under root user:
    
    Shell1:
    VSOMEIP_CONFIGURATION=/vendor/etc/vsomeip/helloworld-local.json \
    VSOMEIP_APPLICATION_NAME=hello_world_service \
    vsomeip-helloworld-service
    
    Shell2:
    VSOMEIP_CONFIGURATION=/vendor/etc/vsomeip/helloworld-local.json \
    VSOMEIP_APPLICATION_NAME=hello_world_client \
    vsomeip-helloworld-client
    
    Tracked-On: OAM-129977
    Signed-off-by: Qi Zhang <qi1.zhang@intel.com>

--- groups/vsomeip/true/product.mk
+++ groups/vsomeip/true/product.mk
@@ -3,3 +3,12 @@ PRODUCT_PACKAGES += \
     libvsomeip3-cfg \
     libvsomeip3-e2e \
     libvsomeip3-sd
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PACKAGES += \
+    vsomeip-helloworld-client \
+    vsomeip-helloworld-service
+
+PRODUCT_COPY_FILES += \
+    external/sdv/vsomeip/examples/hello_world/helloworld-local.json:vendor/etc/vsomeip/helloworld-local.json
+endif
