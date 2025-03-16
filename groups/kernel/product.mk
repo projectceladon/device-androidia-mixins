f499c2ac9b54395b9a73db5ec05087953b7740a0
Yerriswamy KT <yerriswamy.kt@intel.com>
2025-03-16 18:39:56 +0000

    Add support for pre-built kernel based on kleaf build
    As 1st stage, modules built are made as part of vendor_dlkm.
    Once gki is enabled, system_dlkm and vendor_dlkm split will be done.
    Updated init.rc file to load drivers during init
    
    Tests done:
    - Android UI boot
    - Audio playback and recording
    - Video playback and recording
    - Shutdown and reboot
    - Browsing
    - Wifi and Bluetooth connect/disconnect
    
    Tracked-On: OAM-130469
    Signed-off-by: Yerriswamy KT <yerriswamy.kt@intel.com>
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- groups/kernel/product.mk
+++ groups/kernel/product.mk
@@ -3,6 +3,18 @@
   KERNEL_MODULES_ROOT := root/$(KERNEL_MODULES_ROOT_PATH)
 {{/modules_in_bootimg}}
 
+ifeq ($(TARGET_PREBUILT_KERNEL), true)
+ifeq ($(TARGET_BUILD_VARIANT),user)
+PREBUILT_KERNEL_ROOT := vendor/intel/utils_priv/kernel/prebuilts/6.6/{{{prebuilt_kernel_target}}}/user
+else
+PREBUILT_KERNEL_ROOT := vendor/intel/utils_priv/kernel/prebuilts/6.6/{{{prebuilt_kernel_target}}}/userdebug
+endif
+
+PRODUCT_COPY_FILES += \
+    $(PREBUILT_KERNEL_ROOT)/bzImage:kernel
+
+endif
+
 KERNEL_MODULES_ROOT_PATH ?= vendor_dlkm/lib/modules
 KERNEL_MODULES_ROOT ?= $(KERNEL_MODULES_ROOT_PATH)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.vendor.boot.moduleslocation=/$(KERNEL_MODULES_ROOT_PATH)
