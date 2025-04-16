cb1d54523358bc9c21021cf9db8bec7f438969f2
Ankit Agrawal <ankit.agarwal@intel.com>
2025-04-16 16:45:05 +0530

    Removed Instrument cluster configuration for IASW.
    Removing instrument cluster configuration from common project.
    Having targeted only for base_aaos target as diff patch.
    
    Tests done: Device is booted successfully. Showing cluster app only for
    base_aaos target.
    
    Tracked-On: OAM-131887
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- groups/googleservice/gas/product.mk
+++ groups/googleservice/gas/product.mk
@@ -2,8 +2,4 @@ FLAG_GAS_AVAILABLE ?= true
 ifeq ($(FLAG_GAS_AVAILABLE),true)
 $(call inherit-product-if-exists, vendor/google/gas/products/gms.mk)
 
-#Cluster config overlay package
-PRODUCT_PACKAGES += \
-    ClusterConfigOverlay
-
 endif
