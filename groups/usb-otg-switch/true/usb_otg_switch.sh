b939d827ba1c707099c06e5898528bd4de7de33f
Qiang4 Zhang <qiang4.zhang@intel.com>
2025-04-17 14:08:42 +0800

    usb: adapt otg switch config to new role switch driver
    New role switch driver exposes a port sysfs file and the platform device
    name includes a dynamically allocated id, e.g. intel_xhci_usb_sw.1.auto
    
    Assumptions:
    
    Currently only one role switch device is allowed which means only one
    passthrough or virtual xhci controller. This assumption is similar to the
    device mode controller name dwc3.0.auto.
    These platform device IDs are allocated dynamically and may change with
    a different board device configuration.
    
    Tracked-On: OAM-131966
    Signed-off-by: Qiang4 Zhang <qiang4.zhang@intel.com>

--- groups/usb-otg-switch/true/usb_otg_switch.sh
+++ groups/usb-otg-switch/true/usb_otg_switch.sh
@@ -7,19 +7,21 @@ if [[ $1 = "h" ]]; then
   case "$KERNEL_VERSION" in
     4.4*) echo h > /sys/bus/platform/devices/intel-cht-otg.0/mux_state ;;
     4.9*) echo h > /sys/bus/platform/drivers/intel_usb_dr_phy/intel_usb_dr_phy.0/mux_state ;;
-       *) echo "host" > /sys/class/usb_role/intel_xhci_usb_sw-role-switch/role ;;
+    *) for role in `ls /sys/bus/platform/devices/intel_xhci_usb_sw*/port`; do echo $2 > $role; done
+     for role in `ls /sys/class/usb_role/intel_xhci_usb_sw*-role-switch/role`; do echo host > $role; done;;
   esac
 
 elif [[ $1 = "p" ]]; then
   case "$KERNEL_VERSION" in
     4.4*) echo p > /sys/bus/platform/devices/intel-cht-otg.0/mux_state ;;
     4.9*) echo p > /sys/bus/platform/drivers/intel_usb_dr_phy/intel_usb_dr_phy.0/mux_state ;;
-       *) echo "device" > /sys/class/usb_role/intel_xhci_usb_sw-role-switch/role ;;
+    *) for role in `ls /sys/bus/platform/devices/intel_xhci_usb_sw*/port`; do echo $2 > $role; done
+     for role in `ls /sys/class/usb_role/intel_xhci_usb_sw*-role-switch/role`; do echo device > $role; done;;
   esac
 
 else
   echo "Please input h to swith to USB OTG mode"
-  echo "usb_otg_switch.sh h"
+  echo "usb_otg_switch.sh h <port>"
   echo "Please input p to swith USB device mode"
-  echo "usb_otg_switch.sh p"
+  echo "usb_otg_switch.sh p <port>"
 fi
