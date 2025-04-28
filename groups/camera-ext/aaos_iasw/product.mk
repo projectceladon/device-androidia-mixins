44609b622013ed117bf947cb4d5a548189f7530a
shivasku82 <shiva.kumara.rudrappa@intel.com>
2025-04-28 08:25:54 +0530

    add node creation for uvcvideo and virtio
    Issue-Detailed: Renamed virtiocamera device to virtvideo.
    
    Issue-Fixed: add the sepolicy for /dev/virtvideo* device.
    
    Tested-On: Camera preview/capture/record works.
    
    Tracked-On: OAM-132149
    Signed-off-by: shivasku82 <shiva.kumara.rudrappa@intel.com>

---
+++ groups/camera-ext/aaos_iasw/product.mk
@@ -0,0 +1,20 @@
+# Camera: Device-specific configuration files.
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:vendor/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:vendor/etc/permissions/android.hardware.camera.xml
+
+# External camera service
+
+PRODUCT_PACKAGES += android.iacamera.provider@2.4-ivi-service \
+                    android.ia.hardware.camera.provider@2.4-ivi \
+                    android.hardware.camera.provider@2.4-impl-ia
+
+PRODUCT_PACKAGES += android.ia.hardware.camera.provider-ivi \
+					android.iacamera.provider-ivi-service
+
+PRODUCT_PACKAGES += Camera2
+PRODUCT_PACKAGES += MultiCameraApp
+
+PRODUCT_PACKAGES += yavta
+PRODUCT_PACKAGES += media-ctl
+
