e9e4695adfbf770ec2587cdecae9e16145e8ca7c
Tiwari, Divyanshu <divyanshu.tiwari@intel.com>
2025-05-13 11:30:54 +0000

    Required media profiles for camera application
    Added Media_Profiles_V1_0.xml to provide the required
    resolution
    
    Tracked-On: OAM-132417
    Signed-off-by: Tiwari, Divyanshu <divyanshu.tiwari@intel.com>

--- groups/camera-ext/ext-camera-only/product.mk
+++ groups/camera-ext/ext-camera-only/product.mk
@@ -3,7 +3,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.autofocus.xml:vendor/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.external.xml:vendor/etc/permissions/android.hardware.camera.external.xml \
     $(LOCAL_PATH)/{{_extra_dir}}/external_camera_config.xml:vendor/etc/external_camera_config.xml
-
+    $(LOCAL_PATH)/{{_extra_dir}}/media_profiles_V1_0.xml:vendor/etc/media_profiles_V1_0.xml
 
 # External camera service
 PRODUCT_PACKAGES += android.hardware.camera.provider-V1-external-service
--- groups/camera-ext/true/files.spec
+++ groups/camera-ext/true/files.spec
@@ -1,2 +1,3 @@
 [extrafiles]
 external_camera_config.xml: "external camera parameters"
+media_profiles_V1_0.xml: "media profiles parameters"
