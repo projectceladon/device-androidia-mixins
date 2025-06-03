172cbd35ec73cd450d382e1499ba08c7909dddaa
Gang G Chen <gang.g.chen@intel.com>
2025-06-03 21:55:52 +0800

    Generate boot-debug.img in non-GKI config
    For Generic Kernel Image (GKI) using devices that have a vendor_boot
    partition, boot-debug.img mustn't be flashed, as the boot partition
    must be flashed with a certified GKI image.
    Instead vendor_boot-debug.img should be flashed onto the vendor_boot
    partition in order to facilitate debug ramdisk.
    
    Test Done:
    Boot
    
    Tracked-On: OAM-132990
    Signed-off-by: Gang G Chen <gang.g.chen@intel.com>

--- groups/init-boot/true/BoardConfig.mk
+++ groups/init-boot/true/BoardConfig.mk
@@ -22,3 +22,10 @@ BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE :=
 BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+
+# disable debug boot image for GSI testing
+# For Generic Kernel Image (GKI) using devices that have a vendor_boot partition,
+# boot-debug.img mustn't be flashed, as the boot partition must be flashed with
+# a certified GKI image. Instead vendor_boot-debug.img should be flashed
+# onto the vendor_boot partition in order to facilitate debug ramdisk.
+PRODUCT_BUILD_DEBUG_BOOT_IMAGE := false
