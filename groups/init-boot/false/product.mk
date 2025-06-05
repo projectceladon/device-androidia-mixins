75a526dc501b5dcedf6c8fac12b8f813396c375a
Gang G Chen <gang.g.chen@intel.com>
2025-06-05 15:02:55 +0800

    Add init_boot to GPT images
    boot-debug.img will not be generate if enables init_boot image
    
    Test Done:
    Boot
    
    Tracked-On: OAM-132990
    Signed-off-by: Gang G Chen <gang.g.chen@intel.com>

--- groups/init-boot/false/product.mk
+++ groups/init-boot/false/product.mk
@@ -1,13 +1,6 @@
-{{#initboot_enable}}
-INITBOOT_ENABLE := true
-{{/initboot_enable}}
-{{^initboot_enable}}
-INITBOOT_ENABLE := false
-{{/initboot_enable}}
-
 # disable debug boot image for GSI testing
 # For Generic Kernel Image (GKI) using devices that have a vendor_boot partition,
 # boot-debug.img mustn't be flashed, as the boot partition must be flashed with
 # a certified GKI image. Instead vendor_boot-debug.img should be flashed
 # onto the vendor_boot partition in order to facilitate debug ramdisk.
-PRODUCT_BUILD_DEBUG_BOOT_IMAGE := false
+PRODUCT_BUILD_DEBUG_BOOT_IMAGE := true
