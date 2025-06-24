9b428061fa138a90feaef3bd6d846057e0dd05ef
Zhenlong Z Ji <zhenlong.z.ji@intel.com>
2025-06-24 13:26:58 +0800

    Fix the treble violations for bluetooth
    1. Based on treble requirements, vendor defined properties
    should be prefixed with vendor, or the property will be
    by default a system property which cannot be accessed by
    vendor domains.
    2. /data/misc/dhcp is already created in system etc init.rc,
    and it belongs to system data file which cannot be modified
    or created by vendor domains.
    
    Test done: Bluetooth function works normally.
    
    Tracked-On: OAM-133295
    Signed-off-by: Zhenlong Z Ji <zhenlong.z.ji@intel.com>

--- groups/bluetooth/auto/product.mk
+++ groups/bluetooth/auto/product.mk
@@ -12,7 +12,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:vendor/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:vendor/etc/permissions/android.hardware.bluetooth_le.xml
 
-PRODUCT_PROPERTY_OVERRIDES += bluetooth.rfkill=1
+PRODUCT_PROPERTY_OVERRIDES += vendor.bluetooth.rfkill=1
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service.vbt \
--- groups/bluetooth/bcm4339/product.mk
+++ groups/bluetooth/bcm4339/product.mk
@@ -7,7 +7,7 @@ PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:
 		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:vendor/etc/permissions/android.hardware.bluetooth_le.xml
 
 PRODUCT_PROPERTY_OVERRIDES += vendor.bluetooth.hwcfg=stop \
-                bluetooth.rfkill=1
+                vendor.bluetooth.rfkill=1
 
 # Bluetooth eng / userdebug
 ifneq ($(TARGET_BUILD_VARIANT),user)
